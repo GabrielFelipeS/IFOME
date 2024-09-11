19f5dd46f65ea2e6a13bd256323ec5ce9345e99e
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-11 09:48:40 -0300

    chore: initial structure Spring

---
+++ back/src/test/java/br/com/ifsp/ifome/ifome/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.ifome.ifome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
