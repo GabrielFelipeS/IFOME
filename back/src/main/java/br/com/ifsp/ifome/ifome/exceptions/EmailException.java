463adbcc120d9303180b2ba617a681254459497a
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-12 23:24:15 -0300

    remove: EmailException.java

--- back/src/main/java/br/com/ifsp/ifome/ifome/exceptions/EmailException.java
+++
@@ -1,7 +0,0 @@
-package br.com.ifsp.ifome.ifome.exceptions;
-
-public class EmailException extends  RuntimeException {
-    public EmailException() {
-        super("E-mail já registrado");
-    }
-}
--- back/src/main/java/br/com/ifsp/ifome/ifome/infra/ClientExceptionHandler.java
+++ back/src/main/java/br/com/ifsp/ifome/ifome/infra/ClientExceptionHandler.java
@@ -1,17 +1,9 @@
 package br.com.ifsp.ifome.ifome.infra;
 
-import br.com.ifsp.ifome.ifome.exceptions.EmailException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ClientExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(EmailException.class)
-    private ResponseEntity<String> emailExceptionHandler(EmailException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-    }
 }
