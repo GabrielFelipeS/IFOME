70518cc6f66fe02c5f98519c1e0d36d0e3cab7b5
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-13 12:01:16 -0300

    tests: should return all validation errors when creating client

--- back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
+++ back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
@@ -1,7 +1,7 @@
 package br.com.ifsp.ifome.controllers;
 
 
-import br.com.ifsp.ifome.ifome.dto.request.ClientRequest;
+import br.com.ifsp.ifome.dto.request.ClientRequest;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -24,12 +25,14 @@ public class ClienteControllerIT {
     private TestRestTemplate restTemplate;
 
     @Test
+    @DirtiesContext
     @DisplayName("should be possible to create a new client")
     public void shouldBeAbleToCreateANewClient() {
-        ClientRequest client = new ClientRequest("teste@teste.com", "password", "password",
-            LocalDate.now(), "cpf", "Casa", "cep", "endereco", "payment_methods");
+        ClientRequest client = new ClientRequest("teste@teste.com", "@Password1", "password",
+            LocalDate.now().minusYears(18), "48608678071", "Casa", "cep", "endereco", "payment_methods");
 
         ResponseEntity<String> response = restTemplate.postForEntity("/client", client, String.class);
+        System.out.println(response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         DocumentContext document = JsonPath.parse(response.getBody());
@@ -54,10 +57,11 @@ public class ClienteControllerIT {
     }
 
     @Test
+    @DirtiesContext
     @DisplayName("should not be possible to create a new client with already registered email")
     public void shouldReturnErrorWhenCreatingClientWithAlreadyRegisteredEmail() {
-        ClientRequest client = new ClientRequest("user1@gmail.com", "password", "password",
-            LocalDate.now(), "cpf", "Casa", "cep", "endereco", "payment_methods");
+        ClientRequest client = new ClientRequest("user1@gmail.com", "@Password1", "password",
+            LocalDate.now().minusYears(18), "019.056.440-78", "Casa", "cep", "endereco", "payment_methods");
 
         ResponseEntity<String> response = restTemplate.postForEntity("/client", client, String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -66,6 +70,45 @@ public class ClienteControllerIT {
         List<String> message = documentContext.read("$.email");
 
         assertThat(message).containsExactlyInAnyOrder("E-mail já registrado");
+    }
+
+    @Test
+    @DirtiesContext
+    @DisplayName("")
+    public void shouldReturnAllValidationErrorsWhenCreatingClient() {
+        ClientRequest client = new ClientRequest("email@.com", " ", "confirmationPassword",
+            LocalDate.now().plusYears(1), "cpf", "typeResidence", "cep", "address", "payment_methods");
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/client", client, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        System.out.println(response.getBody());
+        List<String> emailErrors = documentContext.read("$.email");
+        assertThat(emailErrors).containsExactlyInAnyOrder("E-mail inválido");
+
+        List<String> passwordErrors = documentContext.read("$.password");
+        assertThat(passwordErrors)
+            .containsExactlyInAnyOrder(
+                "Senha é obrigatório",
+                "A senha precisa possui pelo menos 6 caracteres",
+                "Senha precisa conter pelo menos um número",
+                "Senha precisa conter pelo menos um caractere minúsculo",
+                "Senha precisa conter pelo menos um caractere maiúsculo",
+                "Senha precisa conter pelo menos um caractere especial",
+                "Senha não pode conter espaço");
+
+        List<String> dateOfBirth = documentContext.read("$.dateOfBirth");
+        assertThat(dateOfBirth)
+            .containsExactlyInAnyOrder(
+                "Data de nascimento deve estar no passado",
+                "É necessário ter pelo menos 18 anos"
+            );
+
+        List<String> cpf = documentContext.read("$.cpf");
+        assertThat(cpf)
+            .containsExactlyInAnyOrder(
+                "CPF inválido"
+            );
     }
 
 }
