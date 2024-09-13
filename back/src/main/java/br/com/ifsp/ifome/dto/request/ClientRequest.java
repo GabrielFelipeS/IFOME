e335b6b5bef0dbb4721b348709eeedf18648c4dd
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-13 12:02:10 -0300

    feat: constraint in ClientRequest

--- back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
@@ -1,18 +1,33 @@
 package br.com.ifsp.ifome.dto.request;
 
-import br.com.ifsp.ifome.ifome.validation.anotations.NotRegisteredEmail;
+import br.com.ifsp.ifome.validation.anotations.IsOfLegalAge;
+import br.com.ifsp.ifome.validation.anotations.NotRegisteredEmail;
+import br.com.ifsp.ifome.validation.anotations.ValidPassword;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
-
+// TODO talvez seja necessario inserir a anotação que verifica se password e confirmationPassword são iguais
 public record ClientRequest (
-        @NotBlank(message = "E-mail invalido")
+        @Email(message = "E-mail inválido")
+        @NotBlank(message = "E-mail é obrigatório")
         @NotRegisteredEmail
         String email,
+
+        @ValidPassword
         String password,
         String confirmationPassword,
+
+        @Past(message = "Data de nascimento deve estar no passado")
+        @IsOfLegalAge(message = "É necessário ter pelo menos 18 anos")
         LocalDate dateOfBirth,
+
+        @CPF(message = "CPF inválido")
+        @NotBlank(message = "CPF é obrigatório")
         String cpf,
+
         String typeResidence,
         String cep,
         String address,
