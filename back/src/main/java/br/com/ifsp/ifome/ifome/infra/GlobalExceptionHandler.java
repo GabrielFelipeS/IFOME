f02a6e1d7e39e1d17399a219aa9f7e22b2278221
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-13 08:14:37 -0300

    feat: create GlobalExceptionHandler with handleValidationExceptions

--- back/src/main/java/br/com/ifsp/ifome/ifome/infra/GlobalExceptionHandler.java
+++ back/src/main/java/br/com/ifsp/ifome/ifome/infra/GlobalExceptionHandler.java
@@ -1,17 +1,29 @@
 package br.com.ifsp.ifome.ifome.infra;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    private ResponseEntity<String> constraintViolationHanlder(SQLIntegrityConstraintViolationException exception) {
-        return ResponseEntity.unprocessableEntity().body(exception.getLocalizedMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+        MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
+
 }
