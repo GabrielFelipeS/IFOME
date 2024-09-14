561aa910c104ef3f0d92f8b494b15b331e21cf42
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 13:30:17 -0300

    feat: add validation in AddressRequest

--- back/src/main/java/br/com/ifsp/ifome/dto/request/AddressRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/AddressRequest.java
@@ -1,16 +1,36 @@
 package br.com.ifsp.ifome.dto.request;
 
+import br.com.ifsp.ifome.validation.anotations.CEP;
+import jakarta.validation.constraints.NotBlank;
+
 public record AddressRequest(
+    @NotBlank
+    @CEP
     String cep,
+
+    @NotBlank
     String neighborhood,
+
+    @NotBlank
     String city,
+
+    @NotBlank
     String state,
+
+    @NotBlank
     String address,
+
+    @NotBlank
     String zipCode,
+
+    @NotBlank
     String complement,
 
+    @NotBlank
     String typeResidence,
+
+    @NotBlank
     String number,
-    String walk,
+
     String details
 ) { }
