d1f2e21c31691c8342464221fd8850484d4d34f1
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 19:37:56 -0300

    fix: ConfirmartionPasswordEqualsPasswordValidator

--- back/src/main/java/br/com/ifsp/ifome/validation/validators/ConfirmartionPasswordEqualsPasswordValidator.java
+++ back/src/main/java/br/com/ifsp/ifome/validation/validators/ConfirmartionPasswordEqualsPasswordValidator.java
@@ -1,22 +1,31 @@
 package br.com.ifsp.ifome.validation.validators;
 
 import br.com.ifsp.ifome.dto.request.ClientRequest;
+import br.com.ifsp.ifome.dto.request.RestaurantRequest;
 import br.com.ifsp.ifome.validation.anotations.ConfirmartionPasswordEqualsPassword;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class ConfirmartionPasswordEqualsPasswordValidator implements ConstraintValidator<ConfirmartionPasswordEqualsPassword, ClientRequest> {
+public class ConfirmartionPasswordEqualsPasswordValidator implements ConstraintValidator<ConfirmartionPasswordEqualsPassword, Object> {
     @Override
     public void initialize(ConfirmartionPasswordEqualsPassword constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(ClientRequest clientRequest, ConstraintValidatorContext constraintValidatorContext) {
-        String password = clientRequest.password();
-        String confirmationPassword = clientRequest.confirmationPassword();
+    public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
+        if(object == null) return false;
 
-        if(password == null || confirmationPassword == null) {
+        String password = "";
+        String confirmationPassword = "";
+
+        if(object instanceof ClientRequest clientRequest) {
+            password = clientRequest.password();
+            confirmationPassword = clientRequest.confirmationPassword();
+        } else if(object instanceof RestaurantRequest restaurantRequest) {
+            password = restaurantRequest.password();
+            confirmationPassword = restaurantRequest.confirmationPassword();
+        } else {
             return false;
         }
 
