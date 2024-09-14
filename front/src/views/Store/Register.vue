da936c7bec40e318e64cee8be7b0e22785d47ca8
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-14 20:03:58 -0300

    style: Update Register.vue to add background image and apply responsive styling

--- front/src/views/Store/Register.vue
+++ front/src/views/Store/Register.vue
@@ -4,5 +4,21 @@ import Header from '@/components/store/Header.vue';
 </script>
 
 <template>
-    <Header />
+    <div class="content">
+        <Header />
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.content {
+    @apply w-full h-screen flex flex-col;
+    background-image: url('../../assets/img/store/background_register_store_mob.jpg');
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    @media (min-width: 768px) {
+        background-image: url('../../assets/img/store/background_register_store.webp');
+    }
+}
+</style>
