98ae5714cbb7087e4f3187877a84dbf77c67f2f0
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-14 18:28:30 -0300

    refactor: Update tailwind.config.js to extend theme with new font and colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: true,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'default': ['Inter', 'sans-serif'],
+      },
+    },
+    colors:{
+      "primary": "#E61B1B",
+      "secondary": "#FFF787",
+    }
   },
   variants: {
     extend: {},
