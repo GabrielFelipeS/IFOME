70731ac4b6667f6c87f5e230a798406b8c03fd99
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 13:29:31 -0300

    feat: add casting valid

--- back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
@@ -3,6 +3,7 @@ package br.com.ifsp.ifome.dto.request;
 import br.com.ifsp.ifome.validation.anotations.MinAgeToUse;
 import br.com.ifsp.ifome.validation.anotations.NotRegisteredEmail;
 import br.com.ifsp.ifome.validation.anotations.ValidPassword;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -22,7 +23,7 @@ public record ClientRequest (
         String confirmationPassword,
 
         @Past(message = "Data de nascimento deve estar no passado")
-        @MinAgeToUse(minAge = 14, message = "É necessário ter pelo menos 18 anos")
+        @MinAgeToUse(minAge = 14, message = "É necessário ter pelo menos 13 anos")
         @NotNull(message = "Data de nascimento é obrigatório")
         LocalDate dateOfBirth,
 
@@ -30,6 +31,7 @@ public record ClientRequest (
         @NotBlank(message = "CPF é obrigatório")
         String cpf,
 
-       AddressRequest address,
+        @Valid
+        AddressRequest address,
         String paymentMethods
 ) { }
