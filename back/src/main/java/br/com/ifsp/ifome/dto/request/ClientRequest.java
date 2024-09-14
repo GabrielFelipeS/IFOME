66d06a1dd7851521dfa9267c26aa301beb824f3b
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 11:48:34 -0300

    feat: MinAgeToUse annotation

--- back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
@@ -1,10 +1,11 @@
 package br.com.ifsp.ifome.dto.request;
 
-import br.com.ifsp.ifome.validation.anotations.IsOfLegalAge;
+import br.com.ifsp.ifome.validation.anotations.MinAgeToUse;
 import br.com.ifsp.ifome.validation.anotations.NotRegisteredEmail;
 import br.com.ifsp.ifome.validation.anotations.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -21,7 +22,8 @@ public record ClientRequest (
         String confirmationPassword,
 
         @Past(message = "Data de nascimento deve estar no passado")
-        @IsOfLegalAge(message = "É necessário ter pelo menos 18 anos")
+        @MinAgeToUse(minAge = 14, message = "É necessário ter pelo menos 18 anos")
+        @NotNull(message = "Data de nascimento é obrigatório")
         LocalDate dateOfBirth,
 
         @CPF(message = "CPF inválido")
