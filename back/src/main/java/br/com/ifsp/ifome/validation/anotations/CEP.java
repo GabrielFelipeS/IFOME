c21da4136643406a867cf80a11f5f6c0631fcdf9
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 13:32:23 -0300

    feat: CEP annotation

---
+++ back/src/main/java/br/com/ifsp/ifome/validation/anotations/CEP.java
@@ -0,0 +1,20 @@
+package br.com.ifsp.ifome.validation.anotations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Pattern;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = {})
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Pattern(regexp = "^\\d{5}-\\d{3}$")
+public @interface CEP {
+    String message() default "CEP inválido";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
