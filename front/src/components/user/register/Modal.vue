fa84289433175ae43cf4765caf315f0764bd4598
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-15 20:10:12 -0300

    style: shortened y-axis margin

--- front/src/components/user/register/Modal.vue
+++ front/src/components/user/register/Modal.vue
@@ -6,7 +6,7 @@
 
 <style scoped>
 .body {
-	@apply bg-white flex flex-col w-full my-[5%] mx-5 py-5 justify-center;
+	@apply bg-white flex flex-col w-full my-[2%] mx-5 py-5 justify-center;
 	@apply rounded-2xl border-2 border-b-stone-400;
 
 }
