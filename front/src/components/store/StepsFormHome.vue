aee7ac054304003c6169a35f8ea4d015e903acc6
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-14 22:00:04 -0300

    feat: Add StepsFormHome component to Register.vue

---
+++ front/src/components/store/StepsFormHome.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderSteps from './HeaderSteps.vue';
+</script>
+
+<template>
+    <div class="steps">
+        <HeaderSteps />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.steps{
+    @apply fixed top-0 left-0 w-full h-full bg-white;
+}
+
+</style>
