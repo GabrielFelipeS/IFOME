e9d3d6fc8472c15fa551fcb1bb90056e9fc2883c
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-15 13:12:20 -0300

    feat: add alt attribute to <img> properties

--- front/src/components/user/Header.vue
+++ front/src/components/user/Header.vue
@@ -7,7 +7,7 @@ import Button from '@/components/Button.vue';
 
 <template>
 	<header class="header">
-		<img src="../../assets/img/logo_header.png" class="w-[25%] hidden">
+		<img src="../../assets/img/logo_header.png" class="w-[25%] hidden" alt="Logo">
 		<div class="flex flex-row gap-4 w-[75%] items-center justify-around h-full">
 			<Button href="#">Entrar No Portal</Button>
 			<Button href="#" class="h-full w-[50%] flex items-center justify-center border-b-2 border-primary"
