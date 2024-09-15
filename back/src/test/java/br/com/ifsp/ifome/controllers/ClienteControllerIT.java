642bca3cb3260d4f3ec2a446f5ca3d264351670c
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 23:23:43 -0300

    feat: @WithMockerUser

--- back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
+++ back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
@@ -21,6 +22,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@WithMockUser
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ClienteControllerIT {
 
