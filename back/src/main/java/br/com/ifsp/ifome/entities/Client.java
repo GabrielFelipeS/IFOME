d2c36c20f882cb475b27b5253a37c0102235d237
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-15 11:39:17 -0300

    feat: UserDetails Client

--- back/src/main/java/br/com/ifsp/ifome/entities/Client.java
+++ back/src/main/java/br/com/ifsp/ifome/entities/Client.java
@@ -2,15 +2,18 @@ package br.com.ifsp.ifome.entities;
 
 import br.com.ifsp.ifome.dto.request.ClientRequest;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "clients")
-public class Client {
+public class Client implements UserDetails  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -71,8 +74,38 @@ public class Client {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
     public void setPassword(String password) {
