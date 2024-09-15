86b0d4ccbd495fabc52cdcfb360f52ddba810d9a
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-15 13:34:49 -0300

    feat: new method in ClientService

--- back/src/main/java/br/com/ifsp/ifome/repositories/ClientRepository.java
+++ back/src/main/java/br/com/ifsp/ifome/repositories/ClientRepository.java
@@ -3,6 +3,9 @@ package br.com.ifsp.ifome.repositories;
 import br.com.ifsp.ifome.entities.Client;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends CrudRepository<Client, Long> {
     boolean existsByEmail(String email);
+    Optional<Client> findByEmail(String email);
 }
--- back/src/main/java/br/com/ifsp/ifome/services/ClientService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/ClientService.java
@@ -1,22 +1,41 @@
 package br.com.ifsp.ifome.services;
 
+import br.com.ifsp.ifome.dto.request.ClientLoginRequest;
 import br.com.ifsp.ifome.dto.request.ClientRequest;
 import br.com.ifsp.ifome.dto.response.ClientResponse;
 import br.com.ifsp.ifome.entities.Client;
 import br.com.ifsp.ifome.repositories.ClientRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
     private final ClientRepository clientRepository;
-
-    public ClientService(ClientRepository clientRepository) {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public ClientService(ClientRepository clientRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.clientRepository = clientRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public ClientResponse create(ClientRequest clientRequest) {
-        Client client = new Client(clientRequest);
+        Client client = new Client(clientRequest, bCryptPasswordEncoder);
+        System.out.println(client.getPassword());
         client = clientRepository.save(client);
         return new ClientResponse(client);
+    }
+
+    public Client login(ClientLoginRequest clientLoginRequest) {
+        Optional<Client> client = clientRepository.findByEmail(clientLoginRequest.email());
+
+        if(client.isEmpty() || client.get().isLoginIncorrect(clientLoginRequest, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("email or password is invalid!");
+        }
+
+
+
+        return client.orElseThrow(RuntimeException::new);
     }
 }
