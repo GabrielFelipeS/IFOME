1e036f0b0cd27d0bf32275607173ebdd81049962
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-14 22:55:15 -0300

    fix: requestMapping

--- back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
+++ back/src/test/java/br/com/ifsp/ifome/controllers/ClienteControllerIT.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
