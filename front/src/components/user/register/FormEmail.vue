684a0e1f2c7b553cb538b26e7a31f42226e81282
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-15 22:15:47 -0300

    feat: add error messages

--- front/src/components/user/register/FormEmail.vue
+++ front/src/components/user/register/FormEmail.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Modal from "@/components/user/register/Modal.vue";
 import Button from "@/components/Button.vue";
+import {onMounted, ref} from "vue";
 
 const props = defineProps({
 	formData: {
@@ -11,11 +12,26 @@ const props = defineProps({
 
 const emit = defineEmits(["next-step"]);
 
+const error = ref('');
+
+
 const nextStep = () => {
-	if (props.formData.email.length > 0) {
+	if (props.formData.email.length === 0) {
+		error.value = 'O campo de e-mail não pode estar vazio';
+	} else if (!props.formData.email.includes('@') || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(props.formData.email)) {
+		error.value = 'Digite um e-mail válido';
+	} else {
+		error.value = '';
 		emit('next-step');
 	}
 }
+onMounted(() => {
+	const emailInput = document.querySelector('#email');
+	document.querySelector('input').addEventListener('click', () => {
+		emailInput.classList.remove('btn-error');
+	});
+});
+
 </script>
 
 <template>
@@ -26,8 +42,9 @@ const nextStep = () => {
 		<div class="main">
 			<div class="form-group">
 				<input type="email" placeholder="Digite o seu e-mail" class="form-input"
-					v-model="formData.email"
+					v-model="formData.email" required id="email"
 				>
+				<p class="invalid-input-text" v-if="error">{{ error }}</p>
 			</div>
 			<div class="btn-container">
 				<Button href="#" @click="$emit('previous-step')" inversed
@@ -40,8 +57,10 @@ const nextStep = () => {
 </template>
 
 <style scoped>
+
 .title {
 	@apply flex flex-row font-semibold pl-4 ;
+
 }
 .main {
 	@apply mt-5
@@ -53,9 +72,17 @@ const nextStep = () => {
 	@apply rounded-md w-full flex justify-center align-middle py-[2%];
 }
 .form-group {
-	@apply flex flex-row justify-center items-center align-baseline mx-3;
+	@apply flex flex-col justify-start items-start mx-3;
 }
 .form-input {
 	@apply rounded-md border border-tertiary-subtle p-2 w-full
 }
+
+.invalid-input-text {
+	@apply font-light text-sm text-red-500 px-2;
+}
+.btn-error {
+	@apply border-primary;
+}
+
 </style>
