ab00abc1f12e91c28eb5090e4579ec631d415d76
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-15 23:09:52 -0300

    style: add desktop style support

--- front/src/components/user/Header.vue
+++ front/src/components/user/Header.vue
@@ -6,7 +6,6 @@ const router = useRouter();
 
 const goToPage = async (page) => {
 	await router.push({ name: page }); // Substitua pelo nome da rota
-	window.location.reload(); // Força o recarregamento da página
 };
 
 const props = defineProps({
@@ -21,9 +20,22 @@ const props = defineProps({
 <template>
 	<header class="header">
 		<div class="flex flex-row gap-4 w-[75%] items-center justify-center h-full" v-if="logo">
-			<div class="flex flex-col justify-center items-center">
-				<img src="../../assets/img/logo_header.png" class="w-[25%]" alt="Logo">
+			<img src="../../assets/img/logo_header.png" class="logo" alt="Logo">
+		</div>
+		<div class="flex flex-row gap-4 w-[75%] items-center justify-between h-full" v-if="!logo">
+			<div>
+				<Button href="#">Login</Button>
 			</div>
+			<div>
+				<Button @click="goToPage('user-register')"
+						class="h-full w-[50%] flex items-center justify-center border-b-2 border-primary"
+						inversed href="#">Cadastrar</Button>
+			</div>
+		</div>
+	</header>
+	<header class="header-desktop">
+		<div class="flex flex-row gap-4 w-[75%] items-center justify-center h-full" v-if="logo">
+			<img src="../../assets/img/logo_header.png" class="logo" alt="Logo">
 		</div>
 		<div class="flex flex-row gap-4 w-[75%] items-center justify-between h-full" v-if="!logo">
 			<div>
@@ -39,7 +51,27 @@ const props = defineProps({
 </template>
 
 <style lang="scss" scoped>
-.header{
-	@apply w-full h-[70px] bg-white flex flex-row  justify-center items-center rounded-b-3xl font-default text-lg;
+	* {
+		@apply max-w-full
+	}
+
+	.header {
+		@apply w-full h-[70px] bg-white flex flex-row  justify-center items-center rounded-b-3xl font-default text-lg border-b-4;
+	}
+	.header-desktop {
+		@apply hidden;
+	}
+
+	.header {
+		@apply md:hidden;
+	}
+
+	.header-desktop {
+		@apply w-full h-[70px] bg-white flex flex-row justify-center items-center font-default border-b-4 text-lg;
+	}
+
+
+.logo {
+	@apply w-[70px] h-[70px]
 }
 </style>
--- front/src/components/user/register/Modal.vue
+++ front/src/components/user/register/Modal.vue
@@ -9,5 +9,6 @@
 	@apply bg-white flex flex-col w-full my-[2%] mx-5 py-5 justify-center;
 	@apply rounded-2xl border-2 border-b-stone-400;
 
+	@apply md:w-[50%]
 }
 </style>
