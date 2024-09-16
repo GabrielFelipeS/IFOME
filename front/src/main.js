f2376f95736316d6b1f5dfcc4104c98ffdf0f765
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-15 23:32:16 -0300

    chore: add Font Awesome Icons with OhVueIcons lib

--- front/src/main.js
+++ front/src/main.js
@@ -6,9 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import * as FaIcons from "oh-vue-icons/icons/fa";
+
+const Fa = Object.values({ ...FaIcons });
+addIcons(...Fa);
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component("v-icon", OhVueIcon);
 
 app.mount('#app')
