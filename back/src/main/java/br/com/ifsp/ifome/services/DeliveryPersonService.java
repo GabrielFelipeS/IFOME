c0ec7a288e1fb263575376a609d216d8e86cdcbb
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-17 15:12:49 -0300

    feat: validatorService in DeliveryPersonService

--- back/src/main/java/br/com/ifsp/ifome/services/DeliveryPersonService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/DeliveryPersonService.java
@@ -2,13 +2,16 @@ package br.com.ifsp.ifome.services;
 
 import br.com.ifsp.ifome.dto.request.DeliveryPersonRequest;
 import br.com.ifsp.ifome.dto.request.LoginRequest;
-import br.com.ifsp.ifome.dto.response.LoginResponse;
 import br.com.ifsp.ifome.dto.response.DeliveryPersonResponse;
+import br.com.ifsp.ifome.dto.response.LoginResponse;
 import br.com.ifsp.ifome.entities.DeliveryPerson;
 import br.com.ifsp.ifome.repositories.DeliveryPersonRepository;
+import br.com.ifsp.ifome.validation.interfaces.Validator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,13 +19,18 @@ public class DeliveryPersonService {
     private final DeliveryPersonRepository deliveryPersonRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final TokenService tokenService;
+    private final ValidatorService<DeliveryPersonRequest> validatorService;
 
-    public DeliveryPersonService(TokenService tokenService, DeliveryPersonRepository deliveryPersonRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public DeliveryPersonService(TokenService tokenService, DeliveryPersonRepository deliveryPersonRepository,
+                                 BCryptPasswordEncoder bCryptPasswordEncoder,
+                                 List<Validator<DeliveryPersonRequest>> validators) {
         this.tokenService = tokenService;
         this.deliveryPersonRepository = deliveryPersonRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.validatorService = new ValidatorService<>(validators);
     }
-    public DeliveryPersonResponse create(DeliveryPersonRequest deliveryPersonRequest){
+    public DeliveryPersonResponse create(DeliveryPersonRequest deliveryPersonRequest) throws MethodArgumentNotValidException {
+        validatorService.isValid(deliveryPersonRequest);
         DeliveryPerson deliveryPerson = new DeliveryPerson(deliveryPersonRequest, bCryptPasswordEncoder);
         deliveryPerson = deliveryPersonRepository.save(deliveryPerson);
         return new DeliveryPersonResponse(deliveryPerson);
