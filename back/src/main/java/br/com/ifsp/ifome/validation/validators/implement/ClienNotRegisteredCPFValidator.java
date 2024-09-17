13033ea3bc8cf344855c699ba04a134ac03e9020
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-17 10:24:42 -0300

    feat: ClienNotRegisteredCPFValidator

--- back/src/main/java/br/com/ifsp/ifome/validation/validators/implement/ClienNotRegisteredCPFValidator.java
+++ back/src/main/java/br/com/ifsp/ifome/validation/validators/implement/ClienNotRegisteredCPFValidator.java
@@ -3,7 +3,13 @@ package br.com.ifsp.ifome.validation.validators.implement;
 import br.com.ifsp.ifome.dto.request.ClientRequest;
 import br.com.ifsp.ifome.repositories.ClientRepository;
 import br.com.ifsp.ifome.validation.interfaces.ClientValidator;
+import org.springframework.stereotype.Component;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
 public class ClienNotRegisteredCPFValidator implements ClientValidator {
 
     private final ClientRepository clientRepository;
@@ -13,7 +19,13 @@ public class ClienNotRegisteredCPFValidator implements ClientValidator {
     }
 
     @Override
-    public boolean isValid(ClientRequest request) {
-        return clientRepository.existsByEmailIgnoreCase(request.cpf());
+    public Optional<Map.Entry<String, String>> isValid(ClientRequest request) {
+
+        boolean exists = clientRepository.existsByCpf(request.cpf());
+        if(exists) {
+            return Optional.of(new AbstractMap.SimpleEntry<>("cpf", "Cpf já cadastrado"));
+        }
+
+        return Optional.empty();
     }
 }
