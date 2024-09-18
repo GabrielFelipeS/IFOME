31edb576e16c0b399b04ea1fdea4cfd1aadab2d7
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-18 15:37:24 -0300

    style: fix Modal component size

--- front/src/components/user/register/Modal.vue
+++ front/src/components/user/register/Modal.vue
@@ -9,6 +9,6 @@
 	@apply bg-white flex flex-col w-full my-[2%] mx-5 py-5 justify-center;
 	@apply rounded-2xl border-2 border-b-stone-400;
 
-	@apply md:w-[50%]
+	@apply md:max-w-[500px] 2xl:max-w-[600px];
 }
 </style>
