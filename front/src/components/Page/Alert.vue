32b772d7ae9bd9c7261dff37dc35b0778dceb759
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-19 20:44:39 -0300

    feat: add button to remove toast

--- front/src/components/Page/Alert.vue
+++ front/src/components/Page/Alert.vue
@@ -1,16 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 import { defineProps } from 'vue';
 
-const props = defineProps(['type', 'message']);
+const props = defineProps(['type', 'message', 'index']);
 
+const isVisible = ref(true);
+
+function closeToast() {
+    isVisible.value = false;
+}
 </script>
 
-
 <template>
-    <div id="toast-success"
-        class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+    <div v-if="isVisible" id="toast-success" :class="props.index"
+        class="flex items-center w-full lg:w-[350px] max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
         role="alert">
-        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8   rounded-lg dark:bg-green-800 dark:text-green-200"
+        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg dark:bg-green-800 dark:text-green-200"
             :class="props.type == 'success' ? 'text-green-500 bg-green-100' : 'text-red-500 bg-red-100'">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                 viewBox="0 0 20 20">
@@ -22,7 +27,7 @@ const props = defineProps(['type', 'message']);
         <div class="ms-3 text-sm font-normal">{{ props.message }}</div>
         <button type="button"
             class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            data-dismiss-target="#toast-success" aria-label="Close">
+            aria-label="Close" @click="closeToast">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
