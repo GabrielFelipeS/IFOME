78d2f69dd12d5ca11ae639370b79efb3dae43f3f
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-21 16:46:16 -0300

    fix: name EmailServiceTest to EmailServiceIT

--- back/src/test/java/br/com/ifsp/ifome/services/EmailServiceIT.java
+++ back/src/test/java/br/com/ifsp/ifome/services/EmailServiceIT.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class EmailServiceTest {
+public class EmailServiceIT {
     private GreenMail greenMail;
 
     @Autowired
