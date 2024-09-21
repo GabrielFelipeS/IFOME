6cb747f872bc154a56f6fb8091c1160e5f6f049f
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-21 20:09:40 -0300

    feat: created home view do delivery toute

---
+++ front/src/views/Delivery/Register.vue
@@ -0,0 +1,36 @@
+<script setup>
+import FormHome from '@/components/Delivery/FormHome.vue';
+import Header from '@/components/Delivery/Header.vue';
+import StepsFormHome from '@/components/Delivery/StepsFormHome.vue';
+import { ref } from 'vue';
+
+const data = ref({});
+
+const submitForm = (formData) => {
+    data.value = formData;
+};
+
+</script>
+
+<template>
+    <div class="content">
+        <Header />
+        <FormHome @submit="submitForm"/>
+        <StepsFormHome :data="data"/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+    @apply w-full h-screen flex flex-col justify-end items-center;
+    background-image: url('../../assets/img/delivery/delivery_man_mob.jpg');
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    @media (min-width: 768px) {
+        background-image: url('../../assets/img/delivery/delivery_man.jpg');
+        @apply flex-row justify-end items-end;
+    }
+}
+</style>
