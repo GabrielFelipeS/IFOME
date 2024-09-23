48ce5b8e748a52ffe1412eaacce6dc7685cd8209
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-22 21:09:38 -0300

    style: add max width 1200px

--- front/src/components/user/Modal.vue
+++ front/src/components/user/Modal.vue
@@ -7,8 +7,7 @@
 <style scoped>
 .body {
 	@apply bg-white flex flex-col w-full my-[2%] mx-5 py-5 justify-center;
-	@apply rounded-2xl border-2 border-soft-border;
 
-	@apply md:max-w-[500px] 2xl:max-w-[600px];
+	@apply md:max-w-[1200px];
 }
 </style>
