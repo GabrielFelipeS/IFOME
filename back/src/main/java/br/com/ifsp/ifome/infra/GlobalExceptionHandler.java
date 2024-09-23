9eb4e741b37aca9bec36096b189e694a8a9f3dc0
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-22 23:12:15 -0300

    feat: handleBadCredentialExceptions

--- back/src/main/java/br/com/ifsp/ifome/infra/GlobalExceptionHandler.java
+++ back/src/main/java/br/com/ifsp/ifome/infra/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.format.DateTimeParseException;
 import java.util.*;
 
 
@@ -62,10 +62,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(DateTimeParseException.class)
-    public void handleValidationExceptions(
-        DateTimeParseException ex) {
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(BadCredentialsException.class)
+    public Map<String, Object> handleBadCredentialExceptions(
+        BadCredentialsException ex) {
         logger.warn(ex.getMessage());
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return response;
     }
 }
