407bf6114f7066bdc63688e41105bdc6d2ec436b
Amanda Capistrano <amandacapistrano@outlook.com>
2024-09-26 11:37:05 -0300

    feat: DishRequest

--- back/src/main/java/br/com/ifsp/ifome/entities/Dish.java
+++ back/src/main/java/br/com/ifsp/ifome/entities/Dish.java
@@ -1,5 +1,6 @@
 package br.com.ifsp.ifome.entities;
 
+import br.com.ifsp.ifome.dto.request.DishRequest;
 import jakarta.persistence.*;
 
 @Entity
@@ -9,7 +10,7 @@ public class Dish {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private long price;
+    private Double price;
     private String dishCategory;
     private String dishImage;
     private String availability;
@@ -18,15 +19,14 @@ public class Dish {
     public Dish() {}
 
     public Dish(DishRequest dishRequest) {
-        this.id = dishRequest.id;
-        this.name = dishRequest.name;
-        this.price = dishRequest.price;
-        this.dishCategory = dishRequest.dishCategory;
-        this.dishImage = dishRequest.dishImage;
-        this.availability = dishRequest.availability;
+        this.name = dishRequest.name();
+        this.price = dishRequest.price();
+        this.dishCategory = dishRequest.dishCategory();
+        this.dishImage = dishRequest.dishImage();
+        this.availability = dishRequest.availability();
     }
 
-    public Dish(Long id, String name, long price, String dishCategory, String dishImage, String availability) {
+    public Dish(Long id, String name, Double price, String dishCategory, String dishImage, String availability) {
         this.id = id;
         this.name = name;
         this.price = price;
