6cef14300776c7265c789f70ca9e564c4d83953c
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-26 22:06:19 -0300

    fix: DashRequest

--- back/src/main/java/br/com/ifsp/ifome/dto/request/DishRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/DishRequest.java
@@ -1,13 +1,14 @@
 package br.com.ifsp.ifome.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record DishRequest(
         @NotBlank (message = "O nome do prato é obrigatório")
         String name,
 
-        @NotBlank(message = "O preço é obrigatório")
+        @NotNull(message = "O preço é obrigatório")
         @Positive(message = "Insira um valor de preço válido.")
         Double price,
 
