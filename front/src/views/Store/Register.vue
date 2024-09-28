2b2d04645264b51eb383a08ff82ba32f0ee6eeb5
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-09-28 16:20:52 -0300

    style: change FormHome alignment to be in order with the other pages

--- front/src/views/Store/Register.vue
+++ front/src/views/Store/Register.vue
@@ -22,7 +22,7 @@ const submitForm = (formData) => {
 
 <style lang="scss" scoped>
 .content {
-    @apply w-full h-screen flex flex-col justify-end items-center;
+    @apply w-full h-screen flex flex-col justify-center items-center px-2.5;
     background-image: url('../../assets/img/store/background_register_store_mob.jpg');
     background-position: top;
     background-size: cover;
@@ -30,7 +30,7 @@ const submitForm = (formData) => {
 
     @media (min-width: 768px) {
         background-image: url('../../assets/img/store/background_register_store.webp');
-        @apply flex-row justify-end items-end;
+        @apply flex-row justify-end items-center;
     }
 }
 </style>
