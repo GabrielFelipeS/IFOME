75e2861919ca17561bc68914b5e162788edbcf8e
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-28 17:03:33 -0300

    feat: h2-console

--- back/src/main/java/br/com/ifsp/ifome/infra/SecurityConfig.java
+++ back/src/main/java/br/com/ifsp/ifome/infra/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(request ->
                 request
                     .requestMatchers("/actuator/health").permitAll()
+                    .requestMatchers("/h2-console").permitAll()
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                     .requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/static/**").permitAll()
