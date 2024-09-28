b119d04041110bc154597d6a2d760e0f4262b2af
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-28 17:16:10 -0300

    refactor: rename RestaurantController and RestaurantControllerIT

---
+++ back/src/main/java/br/com/ifsp/ifome/controllers/AuthRestaurantController.java
@@ -0,0 +1,78 @@
+package br.com.ifsp.ifome.controllers;
+
+import br.com.ifsp.ifome.docs.DocsCreateRestaurant;
+import br.com.ifsp.ifome.docs.DocsRestaurantLogin;
+import br.com.ifsp.ifome.dto.ApiResponse;
+import br.com.ifsp.ifome.dto.request.LoginRequest;
+import br.com.ifsp.ifome.dto.request.RestaurantRequest;
+import br.com.ifsp.ifome.dto.response.RestaurantLoginResponse;
+import br.com.ifsp.ifome.dto.response.RestaurantResponse;
+import br.com.ifsp.ifome.services.FileStorageService;
+import br.com.ifsp.ifome.services.RestaurantService;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+
+@RestController
+
+@MultipartConfig
+@RequestMapping("/api/auth/restaurant")
+public class AuthRestaurantController {
+    private final RestaurantService restaurantService;
+    private final FileStorageService fileStorageService;
+
+    public AuthRestaurantController(RestaurantService restaurantService, FileStorageService fileStorageService){
+        this.restaurantService = restaurantService;
+        this.fileStorageService = fileStorageService;
+    }
+
+    @Transactional
+    @DocsCreateRestaurant
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<RestaurantResponse> create(
+        @RequestPart("file")  MultipartFile multipartFile,
+        @Valid @RequestPart("restaurant") RestaurantRequest restaurantRequest,
+        UriComponentsBuilder ucb)
+        throws IOException, MethodArgumentNotValidException {
+
+        System.out.println( fileStorageService.storeFile(multipartFile));
+        RestaurantResponse restaurantResponse = restaurantService.create(restaurantRequest);
+
+        URI locationOfNewRestaurant = ucb
+            .path("restaurant/{id}")
+            .buildAndExpand(restaurantResponse.id())
+            .toUri();
+        return ResponseEntity.created(locationOfNewRestaurant).body(restaurantResponse);
+    }
+
+    @PostMapping("/login")
+    @DocsRestaurantLogin
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest restaurantLogin) {
+        RestaurantLoginResponse restaurantLoginResponse = restaurantService.login(restaurantLogin);
+        ApiResponse apiResponse = new ApiResponse("sucess", restaurantLoginResponse, "Cliente logado com sucesso");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PostMapping("forgot-password")
+    public void forgotPassword(HttpServletRequest request, @RequestBody @Valid @Email String email) throws Exception{
+        System.err.println(request.getServerName());
+        restaurantService.forgotPassword(request, email);
+    }
+
+    @PostMapping("/change-password")
+    public void changePassword(@RequestBody @Valid @Email String email) throws Exception{
+
+    }
+
+}
