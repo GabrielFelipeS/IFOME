f1359afa58d92b3895c7270399dbdefb208d9d81
Micael Hernandes <micael.hernandes@bloomin.com>
2024-09-29 15:53:38 -0300

    refactor: nesting routes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,40 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Register from '@/views/store/Register.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Register from '@/views/store/Register.vue';
 
+// Definindo as rotas com prefixos aninhados
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/store/register',
-      name: 'store-register',
-      component: Register
+      path: '/store',
+      children: [
+        {
+          path: 'register',
+          name: 'store-register',
+          component: Register,
+        },
+        {
+          path: 'login',
+          name: 'store-login',
+          component: () => import('@/views/store/Login.vue'),
+        },
+      ],
     },
     {
-      path: '/store/login',
-      name: 'store-login',
-      component: () => import('@/views/store/Login.vue'),
+      path: '/user',
+      children: [
+        {
+          path: 'register',
+          name: 'user-register',
+          component: () => import('../views/user/Register.vue'),
+        },
+        {
+          path: 'login',
+          name: 'user-login',
+          component: () => import('../views/user/Login.vue'),
+        },
+      ],
     },
     {
-      path: '/user/register',
-      name: 'user-register',
-      component: () => import('../views/user/Register.vue')
+      path: '/delivery',
+      children: [
+        {
+          path: 'register',
+          name: 'delivery-register',
+          component: () => import('../views/delivery/Register.vue'),
+        },
+        {
+          path: 'login',
+          name: 'delivery-login',
+          component: () => import('../views/delivery/Login.vue'),
+        },
+      ],
     },
-    {
-      path: '/user/login',
-      name: 'user-login',
-      component: () => import('../views/user/Login.vue')
-    },
-	{
-	  path: '/delivery/register',
-	  name: 'delivery-register',
-	  component: () => import('../views/delivery/Register.vue')
-	},
-    {
-      path: '/delivery/login',
-      name: 'delivery-login',
-      component: () => import('../views/delivery/Login.vue')
-    },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
