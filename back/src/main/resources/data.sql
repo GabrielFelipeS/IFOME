d510de9ed86089102e8c95d12befd5e35665743a
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-29 19:27:07 -0300

    fix: data schema

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -15,14 +15,14 @@ INSERT INTO restaurants (
     cep,
     address,
     telephone,
-    opening_hours,
     person_responsible,
     person_responsible_CPF,
     email,
     password,
     payment_methods,
     restaurant_image,
-    bank_account
+    bank_account,
+    role
 ) VALUES (
     'Nome Restaurante',
     '58.911.612/0001-16',
@@ -30,14 +30,14 @@ INSERT INTO restaurants (
     '07070-000',
     'Endereço completo',
     '(11) 1234-5678',
-    '12:00',
     'Nome Responsável',
     '13212312365',
     'email1@email.com',
     '$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5vurqg/otzk/motF9ObAG',
     'Dinheiro, Cartão',
     'imagem_restaurante.jpg',
-    'Banco XYZ, Agência 123, Conta 456789'
+    'Banco XYZ, Agência 123, Conta 456789',
+    'RESTAURANT'
 );
 
 INSERT INTO delivery_person (
