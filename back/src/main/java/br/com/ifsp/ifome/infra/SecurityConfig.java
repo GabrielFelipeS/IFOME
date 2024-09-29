fd9825092dc683bff80f52cef62a30bf0cf5258b
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-09-29 09:24:48 -0300

    feat: requestMatchers restaurant

--- back/src/main/java/br/com/ifsp/ifome/infra/SecurityConfig.java
+++ back/src/main/java/br/com/ifsp/ifome/infra/SecurityConfig.java
@@ -39,10 +39,11 @@ public class SecurityConfig {
             .authorizeHttpRequests(request ->
                 request
                     .requestMatchers("/actuator/health").permitAll()
-                    .requestMatchers("/h2-console").permitAll()
+                    .requestMatchers("/h2-console/**").permitAll()
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                     .requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/static/**").permitAll()
+                    .requestMatchers("/api/restaurant/**").permitAll()
                     .anyRequest().authenticated())
             .csrf(AbstractHttpConfigurer::disable)
             .oauth2ResourceServer(auth02 -> auth02.jwt(Customizer.withDefaults()))
