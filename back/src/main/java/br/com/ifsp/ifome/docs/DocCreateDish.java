d798b4cd1c06e28875bac8e11933472a7e04612b
Amanda Capistrano <amandacapistrano@outlook.com>
2024-10-02 18:38:41 -0300

    DocCreateDish

--- back/src/main/java/br/com/ifsp/ifome/docs/DocCreateDish.java
+++ back/src/main/java/br/com/ifsp/ifome/docs/DocCreateDish.java
@@ -68,34 +68,52 @@ import java.lang.annotation.Target;
 
 @ApiResponses({
         @ApiResponse(responseCode =  "201", description = "Prato cadastrado com sucesso",
-        content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation =  br.com.ifsp.ifome.dto.ApiResponse.class),
-                examples =  @ExampleObject(value = """
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation =  br.com.ifsp.ifome.dto.ApiResponse.class),
+                        examples =  @ExampleObject(
+                                value = """
                         {
-                            "status" : "sucess",
+                            "status" : "success",
                             "data": {
                                 "id": 2,
                                 "name" : "Lasanha",
                                 "description" : "Massa fresca e molho bolonhesa",
                                 "price" : "45.0",
                                 "dishCategory": "Massa",
-                                "dishImage" : "image.jpg"
+                                "dishImage" : "image.jpg",
                                 "availability" : "Disponível"
-                            }
+                            },
+                             "message": "Prato criado com sucesso"
   
                         }
                         """)
-        )),
+                )),
         @ApiResponse(responseCode = "400", description = "invalid request",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation = DishResponse.class),
-                    examples = @ExampleObject(
-                            value = """
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = DishResponse.class),
+                        examples = @ExampleObject(
+                                value = """
                                     {
+                                        "name": [
+                                            "O nome do prato é obrigatório"
+                                        ],
+                                        "description": [
+                                            "A descrição do prato é obrigatória"
+                                        ],
                                         "price" : [
-                                        "O preço deve conter apenas valores númericos e positivos"]
+                                            "O preço deve conter apenas valores númericos e positivos",
+                                            "O preço é obrigatório"
+                                        ],
+                                        "dishCategory" : [
+                                            "A categoria do prato é obrigatória"
+                                        ],
+                                        "availability": [
+                                            "A disponibilidade é obrigatória
+                                         ]
                                     }
-        """)))
+                 """)
+                )
+        )
 })
 
 
