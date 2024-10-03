d6eb5bd56a998224f9c0c901a83091a97bb1d7f4
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-02 22:44:42 -0300

    feat: restaurant by id

--- back/src/main/java/br/com/ifsp/ifome/controllers/RestaurantController.java
+++ back/src/main/java/br/com/ifsp/ifome/controllers/RestaurantController.java
@@ -1,12 +1,11 @@
 package br.com.ifsp.ifome.controllers;
 
 import br.com.ifsp.ifome.dto.response.RestaurantResponse;
+import br.com.ifsp.ifome.exceptions.RestaurantNotFoundException;
 import br.com.ifsp.ifome.repositories.RestaurantRepository;
 import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +23,13 @@ public class RestaurantController {
     @GetMapping
     public ResponseEntity<List<RestaurantResponse>> getAllRestaurant() {
         return ResponseEntity.ok(restaurantRepository.getAllRestaurants());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantResponse> getRestaurantById(@PathVariable Long id) {
+        var restaurant = restaurantRepository.findById(id).orElseThrow(RestaurantNotFoundException::new);
+        var restaurantResponse = RestaurantResponse.from(restaurant);
+        return ResponseEntity.ok(restaurantResponse);
     }
 
 }
