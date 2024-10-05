fcd0c631492e2906e0eed68a656020f8bc145caa
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-05 11:56:11 -0300

    chore: OpenApiConfig

---
+++ back/src/main/java/br/com/ifsp/ifome/infra/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package br.com.ifsp.ifome.infra;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .name("Bearer Token")
+            .type(Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+        return new OpenAPI()
+            .info(new Info().title("API Title").version("1.0.0"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("Bearer Token", securityScheme))
+        ;
+    }
+}
