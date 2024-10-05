b818a3dd35a72d02d5e15734a98d3628f7e561f4
Micael Hernandes <micael.hernandes@bloomin.com>
2024-10-05 12:06:38 -0300

    fix: ajusts final store register icons path

--- front/src/components/store/register/StepsFormHome.vue
+++ front/src/components/store/register/StepsFormHome.vue
@@ -720,11 +720,11 @@ const returnSteps = () => {
 
         <div class="step" v-if="currentStep === 7">
             <div class="final">
-                <img src="../../assets/img/store/shop.png" alt="Logo Ifome">
+                <img src="../../../assets/img/store/shop.png" alt="Logo Ifome">
                 <h2>Seja Bem Vindo, {{ name }}</h2>
-                <img src="../../assets/img/store/sushi.png" alt="Icone Sushi" class="baloon">
-                <img src="../../assets/img/store/hamburguer-de-queijo.png" alt="Icone Hamburguer" class="baloon1">
-                <img src="../../assets/img/store/plait.png" alt="Icone Prato" class="baloon2">
+                <img src="../../../assets/img/store/sushi.png" alt="Icone Sushi" class="baloon">
+                <img src="../../../assets/img/store/hamburguer-de-queijo.png" alt="Icone Hamburguer" class="baloon1">
+                <img src="../../../assets/img/store/plait.png" alt="Icone Prato" class="baloon2">
                 <button type="submit" class="btn-text">Começar a Vender</button>
             </div>
         </div>
