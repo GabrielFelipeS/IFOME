3c54f3b2bbce5daa336e8d9b6d3eb896318dc549
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-12 12:32:20 -0300

    cleanup: remove import not used

--- back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/ClientRequest.java
@@ -1,9 +1,11 @@
 package br.com.ifsp.ifome.dto.request;
 
 import br.com.ifsp.ifome.validation.anotations.*;
-import br.com.ifsp.ifome.validation.anotations.Past;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
