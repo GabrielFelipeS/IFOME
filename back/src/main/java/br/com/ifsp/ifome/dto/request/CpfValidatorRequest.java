90690f69bd7a0cb46e2b4dcf9d8ccd6bdf2b6aff
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-12 14:30:14 -0300

    refactor: messages changed to CpfValidatorRequest

--- back/src/main/java/br/com/ifsp/ifome/dto/request/CpfValidatorRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/CpfValidatorRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record CpfValidatorRequest(
-    @CPF(message = "O campo \"CPF\" deve estar no formato: XXX.XXX.XXX-XX, valor atual: {validatedValue}")
+    @CPF(message = "O campo \"CPF\" deve estar no formato: XXX.XXX.XXX-XX}")
     @NotBlank(message = "O campo \"CPF\" é obrigatório")
     String cpf
 ) {
