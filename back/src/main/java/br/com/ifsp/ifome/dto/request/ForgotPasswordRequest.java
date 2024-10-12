8868d73e1caa41c18d6f1cc4fcc1629eb708f6e8
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-12 14:29:21 -0300

    refactor: messages changed to ForgotPasswordRequest

--- back/src/main/java/br/com/ifsp/ifome/dto/request/ForgotPasswordRequest.java
+++ back/src/main/java/br/com/ifsp/ifome/dto/request/ForgotPasswordRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ForgotPasswordRequest(
-    @NotBlank
-    @Email
+    @NotBlank(message = "O campo \"E-mail\" é obrigatório")
+    @Email(message = "E-mail deve estar no formato: nome@dominio.com")
     String email
 ) {
 }
