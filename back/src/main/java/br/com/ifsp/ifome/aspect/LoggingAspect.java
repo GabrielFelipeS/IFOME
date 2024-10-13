2cad5a1776b9673b726160efe4a33cc9c98475a1
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-13 11:07:32 -0300

    feat: sensiveDataMethods e loginMethods loggers

--- back/src/main/java/br/com/ifsp/ifome/aspect/LoggingAspect.java
+++ back/src/main/java/br/com/ifsp/ifome/aspect/LoggingAspect.java
@@ -1,10 +1,9 @@
 package br.com.ifsp.ifome.aspect;
 
-import org.aspectj.lang.JoinPoint;
+import br.com.ifsp.ifome.dto.request.LoginRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +16,20 @@ import java.util.Arrays;
 public class LoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+//    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *) && !@annotation(br.com.ifsp.ifome.aspect.SensiveData)")
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
     public void controllerMethods() {}
 
+//    @Pointcut("within(@org.springframework.stereotype.Service *) && !@annotation(br.com.ifsp.ifome.aspect.SensiveData)")
     @Pointcut("within(@org.springframework.stereotype.Service *)")
     public void serviceMethods() {}
+
+    @Pointcut("@annotation(br.com.ifsp.ifome.aspect.SensiveData)")
+    public void sensiveDataMethods() {}
+
+    @Pointcut("@annotation(br.com.ifsp.ifome.aspect.Login)")
+    public void loginMethods() {}
+
 
     @Around("controllerMethods()")
     public Object logAroundControllerMethods(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -51,6 +59,39 @@ public class LoggingAspect {
         Object result = joinPoint.proceed();
 
         logger.info("Método executado: {} do service: {} com retorno {}", methodName, ServiceClass, result);
+
+        return result;
+    }
+
+    @Around("sensiveDataMethods()")
+    public Object logAroundSensiveDataMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        String ServiceClass = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        logger.info("Método {} de {} está prestes a ser executado.", methodName, ServiceClass);
+
+        Object result = joinPoint.proceed();
+
+        logger.info("Método executado: {} do service: {} executado com sucesso", methodName, ServiceClass);
+
+        return result;
+    }
+
+    @Around("loginMethods() && args(loginRequest,..)")
+    public Object logAroundLoginMethods(ProceedingJoinPoint joinPoint, LoginRequest loginRequest) throws Throwable {
+        String ServiceClass = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        logger.info("Método {} de {} está prestes a ser executado.", methodName, ServiceClass);
+        logger.info("Tentativa de login para: {}", loginRequest.email());
+
+        Object result = joinPoint.proceed();
+
+        logger.info("Login realizado para: {}", loginRequest.email());
+
+        logger.info("Método executado: {} do service: {} executado com sucesso", methodName, ServiceClass);
 
         return result;
     }
