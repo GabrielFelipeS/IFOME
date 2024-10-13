d465e13093f0c1dc9de0101912e4a6b99c74ff5a
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-13 12:33:23 -0300

    feat: add search from api to autocomplete categories

--- front/src/components/store/dish/new/Form.vue
+++ front/src/components/store/dish/new/Form.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Modal from "@/components/user/Modal.vue";
-import {ref, watch} from "vue";
+import {onMounted, ref, watch} from "vue";
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import {data} from "autoprefixer";
+import axios from "axios";
 
 const selectedFiles = ref([]);
 const errorPhotos = ref(false);
@@ -47,6 +50,25 @@ function removeFile() {
 	selectedFiles.value = [];
 }
 
+const categorias = ref([]);
+async function getCategories() {
+	const apiCategories = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+	apiCategories.data.categories.forEach(category => {
+		categorias.value.push(category.strCategory);
+	});
+}
+getCategories();
+
+function search(input) {
+	console.log(categorias.value);
+	if (!input) {
+		return categorias.value;
+	}
+	return categorias.value.filter(category => {
+		return category.toLowerCase().includes(input.toLowerCase());
+	});
+}
+
 watch(selectedFiles, () => {
 	if (selectedFiles.value.length > 0) {
 		document.getElementById('drag-drop-photo').style.display = 'none';
@@ -69,32 +91,29 @@ watch(selectedFiles, () => {
 				<label for="description">Descrição</label>
 				<input type="text" placeholder="Digite a descrição do prato" class="form-input"
 					   required id="description">
-				<div class="md:columns-2 w-full">
-					<div class="w-full">
-						<label for="price">Valor</label>
-						<input type="text" placeholder="R$ 00,00" class="form-input"
-							   required id="price">
-					</div>
-					<div class="w-full">
-						<label for="category">Categoria</label>
-						<input type="text" placeholder="Selecione uma categoria" class="form-input"
-							   required id="category">
-					</div>
-				</div>
+				<label for="price">Valor</label>
+				<input type="text" placeholder="R$ 00,00" class="form-input"
+					   required id="price">
+				<label for="category">Categoria</label>
+				<autocomplete :search="search" placeholder="Selecione ou digite uma categoria"
+							  class="w-full">
+				</autocomplete>
 				<p class="text-center w-full mb-1 mt-3 font-semibold">Foto do prato</p>
-				<div class="border-2 border-dashed border-gray-300 rounded-lg p-5 w-full text-center h-[20%] flex items-center justify-center"
-					 @dragover.prevent="dragOver" @dragleave.prevent="dragLeave" @drop.prevent="drop" id="drag-drop-photo">
-					<input type="file" id="photo" name="photo" class="hidden" @change="handleFileChange" />
+				<div
+					class="border-2 border-dashed border-gray-300 rounded-lg p-5 w-full text-center h-[20%] flex items-center justify-center"
+					@dragover.prevent="dragOver" @dragleave.prevent="dragLeave" @drop.prevent="drop"
+					id="drag-drop-photo">
+					<input type="file" id="photo" name="photo" class="hidden" @change="handleFileChange"/>
 					<label for="photo" class="cursor-pointer flex flex-col items-center justify-center">
-						<svg data-name="Livello 1" id="Livello_1" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg"
+						<svg data-name="Livello 1" id="Livello_1" viewBox="0 0 128 128"
+							 xmlns="http://www.w3.org/2000/svg"
 							 width="30" height="30" class="mb-2">
-							<title />
+							<title/>
 							<path
-								d="M37.09,32.91A3,3,0,0,0,39.21,32L61,10.24V91a3,3,0,0,0,6,0V10.24L88.79,32A3,3,0,0,0,93,27.79L66.12.88A3,3,0,0,0,65.66.5L65.43.38a3,3,0,0,0-.29-.15,3,3,0,0,0-.31-.1L64.59.06a3,3,0,0,0-1.18,0l-.25.08a2.93,2.93,0,0,0-.31.1,3,3,0,0,0-.29.15L62.34.5a3,3,0,0,0-.46.38L35,27.79a3,3,0,0,0,2.12,5.12Z" />
+								d="M37.09,32.91A3,3,0,0,0,39.21,32L61,10.24V91a3,3,0,0,0,6,0V10.24L88.79,32A3,3,0,0,0,93,27.79L66.12.88A3,3,0,0,0,65.66.5L65.43.38a3,3,0,0,0-.29-.15,3,3,0,0,0-.31-.1L64.59.06a3,3,0,0,0-1.18,0l-.25.08a2.93,2.93,0,0,0-.31.1,3,3,0,0,0-.29.15L62.34.5a3,3,0,0,0-.46.38L35,27.79a3,3,0,0,0,2.12,5.12Z"/>
 							<path
-								d="M125,88a3,3,0,0,0-3,3v22a9,9,0,0,1-9,9H15a9,9,0,0,1-9-9V91a3,3,0,0,0-6,0v22a15,15,0,0,0,15,15h98a15,15,0,0,0,15-15V91A3,3,0,0,0,125,88Z" />
+								d="M125,88a3,3,0,0,0-3,3v22a9,9,0,0,1-9,9H15a9,9,0,0,1-9-9V91a3,3,0,0,0-6,0v22a15,15,0,0,0,15,15h98a15,15,0,0,0,15-15V91A3,3,0,0,0,125,88Z"/>
 						</svg>
-
 						<span>Arraste e solte sua foto aqui</span>
 						<span class="text-sm text-gray-400">ou <span class="text-blue-500">clique aqui</span> e
                     selecione</span>
@@ -114,10 +133,14 @@ watch(selectedFiles, () => {
 			<div class="btn-container">
 				<button type="button" class="button text-primary hover:text-white border border-primary
 					hover:bg-primary active:bg-primary-dark">
-					<v-icon name="fa-times" /> Cancelar</button>
+					<v-icon name="fa-times"/>
+					Cancelar
+				</button>
 				<button type="button" class="button bg-primary border border-primary
 				 	text-white hover:bg-primary-dark active:bg-primary-darker">
-				Salvar prato <v-icon name="fa-save" /></button>
+					Salvar prato
+					<v-icon name="fa-save"/>
+				</button>
 			</div>
 		</div>
 	</Modal>
@@ -128,20 +151,25 @@ watch(selectedFiles, () => {
 	@apply flex flex-row font-semibold pl-4 text-2xl ;
 
 }
+
 .main {
 	@apply mt-5
 }
+
 .btn-container {
 	@apply flex flex-col-reverse justify-center items-center align-baseline mt-10 px-[10%] gap-5;
 	@apply md:flex-row md:gap-10;
 }
+
 .button {
 	@apply rounded-md px-[2%] py-3 w-full flex justify-center items-center gap-3;
 	@apply md:p-[2%];
 }
+
 .form-group {
 	@apply flex flex-col justify-start items-start mx-3 ;
 }
+
 .form-input {
 	@apply rounded-md border border-tertiary-subtle p-2 w-full mb-2;
 
