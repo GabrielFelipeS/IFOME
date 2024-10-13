ae0ef550ef50457bd8e0704626d412ecf0c16490
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-13 13:46:07 -0300

    feat: add inserts

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -8,6 +8,12 @@ VALUES ('Gabriel', 'email1@email.com', '$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5
 INSERT INTO clients (name, email, password, date_of_birth, cpf, role)
 VALUES ('Gabriel', 'noreply.ifome@gmail.com', '$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5vurqg/otzk/motF9ObAG', '2003-04-14', '77025681008',  'CLIENT');
 
+INSERT INTO address (name_address, cep, neighborhood, city, state, address, number, complement, details, type_residence, client_id)
+VALUES
+    ('Endereço João', '05413-020', 'Pinheiros', 'São Paulo', 'SP', 'Rua dos Três Irmãos', '50', 'Apto 101', 'Próximo ao Parque Villa-Lobos', 'Casa', 1),
+    ('Endereço Maria', '01234-567', 'Centro', 'São Paulo', 'SP', 'Avenida São João', '500', NULL, 'Próximo à Praça da República', 'Apartamento', 2),
+    ('Endereço Carlos', '03090-000', 'Mooca', 'São Paulo', 'SP', 'Rua da Mooca', '300', 'Apto 202', 'Perto do Parque da Mooca', 'Casa', 3);
+
 INSERT INTO restaurants (
     name_restaurant,
     cnpj,
@@ -19,8 +25,11 @@ INSERT INTO restaurants (
     password,
     payment_methods,
     restaurant_image,
-    role,
-    is_open
+    bank,
+    agency,
+    account,
+    is_open,
+    role
 ) VALUES (
     'Açai do monge',
     '58911612000116',
@@ -32,36 +41,39 @@ INSERT INTO restaurants (
     '$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5vurqg/otzk/motF9ObAG',
     'Dinheiro, Cartão',
     'image.jpg',
-    'RESTAURANT',
-    false
+    'Banco do Brasil',
+    '1234',
+    '00012345-6',
+    false,
+    'RESTAURANT'
 );
+
+INSERT INTO address (name_address, cep, neighborhood, city, state, address, number, complement, details, type_residence, restaurant_id)
+VALUES
+    ('Endereço Carlos', '03090-000', 'Mooca', 'São Paulo', 'SP', 'Rua da Mooca', '300', 'Apto 202', 'Perto do Parque da Mooca', 'Casa', 1);
+
+INSERT INTO opening_hours (id, day_of_the_week, opening, closing, restaurant_id) VALUES
+     (1, 'Segunda-feira', '08:00', '22:00', 1),
+     (2, 'Terça-feira', '08:00', '22:00', 1),
+     (3, 'Quarta-feira', '08:00', '22:00', 1),
+     (4, 'Quinta-feira', '08:00', '22:00', 1),
+     (5, 'Sexta-feira', '08:00', '23:00', 1),
+     (6, 'Sábado', '09:00', '23:00', 1),
+     (7, 'Domingo', '09:00', '22:00', 1);
 
 INSERT INTO delivery_person (
-    email,
-    password,
-    cpf,
-    name,
-    date_of_birth,
-    type_of_vehicle,
-    plate,
-    telephone,
-    cnh_number,
-    cnh_validity,
-    vehicle_document,
-    role
+    email, password,
+    cpf, name, date_of_birth, type_of_vehicle,
+    plate, telephone,  cnh_number,
+    cnh_validity, vehicle_document, role
 )
 VALUES (
-   'email1@email.com',
-   '$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5vurqg/otzk/motF9ObAG',
-   '52800314028',
-   'João da Silva',
-   '1985-06-15',
-   'carro',
-   'ABC-1234',
-   '(11) 98765-4321',
-   '12345678910',
-   '2026-12-31',
-   '987654321',
-    'DELIVERY'
+    'email1@email.com','$2a$10$rQP2X0ALCvxkpWkUnM/.o.JdpVtVSpQk5vurqg/otzk/motF9ObAG',
+    '52800314028','João da Silva', '1985-06-15','carro',
+    'ABC-1234','(11) 98765-4321','12345678910',
+    '2026-12-31','987654321','DELIVERY'
 );
 
+INSERT INTO address (name_address, cep, neighborhood, city, state, address, number, complement, details, type_residence, delivery_id)
+VALUES
+    ('Endereço Carlos', '03090-000', 'Mooca', 'São Paulo', 'SP', 'Rua da Mooca', '300', 'Apto 202', 'Perto do Parque da Mooca', 'Casa', 1);
