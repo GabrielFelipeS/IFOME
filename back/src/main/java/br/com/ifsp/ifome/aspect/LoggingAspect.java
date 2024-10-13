723e26827dffc4b0d99c78af13f6c9c8bc6dd9bd
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-13 14:57:09 -0300

    fix: logger

--- back/src/main/java/br/com/ifsp/ifome/aspect/LoggingAspect.java
+++ back/src/main/java/br/com/ifsp/ifome/aspect/LoggingAspect.java
@@ -2,9 +2,7 @@ package br.com.ifsp.ifome.aspect;
 
 import br.com.ifsp.ifome.dto.request.LoginRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -31,24 +29,28 @@ public class LoggingAspect {
     public void loginMethods() {}
 
 
-    @Around("controllerMethods()")
-    public Object logAroundControllerMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Before("controllerMethods()")
+    public void logAroundControllerMethods(ProceedingJoinPoint joinPoint) throws Throwable {
         String controllerClass = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
 
         logger.info("Método {} de {} está prestes a ser executado.", methodName, controllerClass);
         logger.info("Método chamado com os argumentos: {}", Arrays.toString(args));
+    }
 
-        Object result = joinPoint.proceed();
+    @AfterReturning(value = "controllerMethods()", returning = "result")
+    public Object logAroundControllerMethods(ProceedingJoinPoint joinPoint, Object result) throws Throwable {
+        String controllerClass = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
 
         logger.info("Método executado: {} do controlador: {} com retorno {}", methodName, controllerClass, result);
-
         return result;
     }
 
-    @Around("serviceMethods()")
-    public Object logAroundServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+
+    @Before("serviceMethods()")
+    public void logAroundServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
         String ServiceClass = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
@@ -56,10 +58,14 @@ public class LoggingAspect {
         logger.info("Método {} de {} está prestes a ser executado.", methodName, ServiceClass);
         logger.info("Método chamado com os argumentos: {}", Arrays.toString(args));
 
-        Object result = joinPoint.proceed();
+    }
 
-        logger.info("Método executado: {} do service: {} com retorno {}", methodName, ServiceClass, result);
+    @AfterReturning(value = "serviceMethods()", returning = "result")
+    public Object logAfterReturningServiceMethods(ProceedingJoinPoint joinPoint, Object result) throws Throwable {
+        String serviceClass = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
 
+        logger.info("Método executado: {} do service: {} com retorno {}", methodName, serviceClass, result);
         return result;
     }
 
@@ -87,12 +93,16 @@ public class LoggingAspect {
         logger.info("Método {} de {} está prestes a ser executado.", methodName, ServiceClass);
         logger.info("Tentativa de login para: {}", loginRequest.email());
 
-        Object result = joinPoint.proceed();
+        try {
+            Object result = joinPoint.proceed();
+            logger.info("Login realizado para: {}", loginRequest.email());
 
-        logger.info("Login realizado para: {}", loginRequest.email());
+            logger.info("Método executado: {} do service: {} executado com sucesso", methodName, ServiceClass);
 
-        logger.info("Método executado: {} do service: {} executado com sucesso", methodName, ServiceClass);
-
-        return result;
+            return result;
+        } catch (Exception e) {
+            logger.info("Login falhou para: {}", loginRequest.email());
+            throw e;
+        }
     }
 }
