5ba6fb41f617a9dc25d750a2011855395349d62f
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-13 14:01:59 -0300

    fix: documentation

--- back/src/main/java/br/com/ifsp/ifome/docs/DocsRestaurantLogin.java
+++ back/src/main/java/br/com/ifsp/ifome/docs/DocsRestaurantLogin.java
@@ -83,27 +83,89 @@ import java.lang.annotation.Target;
         schema = @Schema(implementation = br.com.ifsp.ifome.dto.ApiResponse.class),
         examples =  @ExampleObject(value = """
             {
-                   "status": "sucess",
-                   "data": {
-                     "restaurant": {
-                       "id": 1,
-                       "nameRestaurant": "Nome Restaurante",
-                       "cnpj": "58.911.612/0001-16",
-                       "foodCategory": "Pizzaria",
-                       "address": [],
-                       "telephone": "(11) 1234-5678",
-                       "openingHours": [],
-                       "personResponsible": "Nome Responsável",
-                       "personResponsibleCPF": null,
-                       "email": "email1@email.com",
-                       "paymentMethods": "Dinheiro, Cartão",
-                       "restaurantImage": "imagem_restaurante.jpg",
-                       "bankAccount": null
-                     },
-                     "token": "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJhcGlfaWZvbWUiLCJzdWIiOiJlbWFpbDFAZW1haWwuY29tIiwiZXhwIjoxNzI3MTk3NDMzLCJpYXQiOjE3MjcxOTcxMzN9.IhHau0U76i4wOyq5zwrfy16Af9jkvEejyfyIxG7yIyZDZRhrna9CyDj7QDNvTOpEcFezsXtVTUJtsAjDekrIUZSmTxfoDem_ynyab8Az_wM6Kpno3psamVHJstBtcWBTjcAOGNjqQbR-mGsC_1-0qnV79Yk4XA9rQj4giPRyx9kU6ey6z1HCbIpoqNMhAHCK7RVLto8bN7LMsz5nh00h0xsUng_pzB2WBlUhJ4yjcMPZcu9qPsOlFbeE79SmHAYMLZssLFmJNPBJuuFQPJoUXuMKQH4vFGOoLLYzBkleb0vN3wROVFD2EuHMxYQo2mE8K1MZm_LPCHotxU04pDmDOg"
-                   },
-                   "message": "Logado com sucesso"
-                 }
+              "status": "success",
+              "data": {
+                "restaurant": {
+                  "id": 1,
+                  "nameRestaurant": "Açai do monge",
+                  "cnpj": "58911612000116",
+                  "foodCategory": "Sorveteria",
+                  "address": [
+                    {
+                      "id": 4,
+                      "nameAddress": "Endereço Carlos",
+                      "cep": "03090-000",
+                      "neighborhood": "Mooca",
+                      "city": "São Paulo",
+                      "state": "SP",
+                      "address": "Rua da Mooca",
+                      "number": "300",
+                      "complement": "Apto 202",
+                      "details": "Perto do Parque da Mooca",
+                      "typeResidence": "Casa"
+                    }
+                  ],
+                  "telephone": "(11) 1234-5678",
+                  "openingHours": [
+                    {
+                      "id": 1,
+                      "dayOfTheWeek": "Segunda-feira",
+                      "opening": "08:00",
+                      "closing": "22:00"
+                    },
+                    {
+                      "id": 2,
+                      "dayOfTheWeek": "Terça-feira",
+                      "opening": "08:00",
+                      "closing": "22:00"
+                    },
+                    {
+                      "id": 3,
+                      "dayOfTheWeek": "Quarta-feira",
+                      "opening": "08:00",
+                      "closing": "22:00"
+                    },
+                    {
+                      "id": 4,
+                      "dayOfTheWeek": "Quinta-feira",
+                      "opening": "08:00",
+                      "closing": "22:00"
+                    },
+                    {
+                      "id": 5,
+                      "dayOfTheWeek": "Sexta-feira",
+                      "opening": "08:00",
+                      "closing": "23:00"
+                    },
+                    {
+                      "id": 6,
+                      "dayOfTheWeek": "Sábado",
+                      "opening": "09:00",
+                      "closing": "23:00"
+                    },
+                    {
+                      "id": 7,
+                      "dayOfTheWeek": "Domingo",
+                      "opening": "09:00",
+                      "closing": "22:00"
+                    }
+                  ],
+                  "personResponsible": "Nome Responsável",
+                  "personResponsibleCPF": "07635915053",
+                  "email": "email1@email.com",
+                  "paymentMethods": "Dinheiro, Cartão",
+                  "restaurantImage": "image.jpg",
+                  "bankAccount": {
+                    "bank": "Banco do Brasil",
+                    "agency": "1234",
+                    "account": "00012345-6"
+                  },
+                  "isOpen": false
+                },
+                "token": "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJhcGlfaWZvbWUiLCJzdWIiOiJlbWFpbDFAZW1haWwuY29tIiwiZXhwIjoxNzM0MDk3NDEwLCJpYXQiOjE3Mjg4Mzc4MTAsImF1dGhvcml0aWVzIjpbIlJFU1RBVVJBTlRfUkVBRCIsIlJFU1RBVVJBTlRfREVMRVRFIiwiUkVTVEFVUkFOVF9DUkVBVEUiLCJSRVNUQVVSQU5UX1VQREFURSIsIlJPTEVfUkVTVEFVUkFOVCJdfQ.JmkkQgoZ8gBhQkJT3MGrCpo_8TDhKfzlmti_HFQie-5LSd2Skf31HD8LSY993dh5JVGA5vAbnKI5kYj-R1e-YEb61B5x8LSKzauyX9Dsh4ONSK2hBMSswVjc_WMPqcioWvrSPjWmkod213bwHlq5TdIak6A_6x8NFxGb5PK4ZQgBSMMn6LI_rPuF54pwhHo7KXQjv1piqHYRL39tpRxcd7JFCCqwHVgYadNTwZJQ8U8A2VtqF35zhsrQeeN4CKNdtM9zQKDq8kLWOYMQKtc3z_cXhxeLtWtSLBFC_CALV4Mhthos_tMfml6u4kzwR55hzZY5eS_Dkw-4RtD5aRMaCw"
+              },
+              "message": "Restaurante logado com sucesso"
+            }
             """)
       )),
     @ApiResponse(responseCode = "403", description = "Email ou senha incorretos!",
