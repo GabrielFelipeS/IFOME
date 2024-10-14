30d9f900e2bf96b63dd15e8ba0d797dc6d9405ae
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-14 18:58:38 -0300

    cleanup: restaurant controller

--- back/src/main/java/br/com/ifsp/ifome/controllers/RestaurantController.java
+++ back/src/main/java/br/com/ifsp/ifome/controllers/RestaurantController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
-
 @MultipartConfig
 @RequestMapping("/api/restaurant/")
 public class RestaurantController {
