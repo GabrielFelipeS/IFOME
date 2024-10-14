3edb242256686abfa1d37cb4ec9bc0397e0bde5f
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-14 18:59:56 -0300

    test: create ClientControllerIT

---
+++ back/src/test/java/br/com/ifsp/ifome/controllers/ClientControllerIT.java
@@ -0,0 +1,30 @@
+package br.com.ifsp.ifome.controllers;
+
+import br.com.ifsp.ifome.services.TokenService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClientControllerIT {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private TokenService tokenService;
+
+    private String token;
+
+    @BeforeEach
+    public void setUp() {
+        this.token = tokenService.generateToken("email1@email.com",  List.of(new SimpleGrantedAuthority("ROLE_RESTAURANT")));
+    }
+
+}
