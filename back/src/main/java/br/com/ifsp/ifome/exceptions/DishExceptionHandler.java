a2abb51e3ab54a6d30176914608540262a7e43f4
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-14 23:41:28 -0300

    feat: DishExceptionHandler, DishNotFoundException e ClientNotFoundException

---
+++ back/src/main/java/br/com/ifsp/ifome/exceptions/DishExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.ifsp.ifome.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class DishExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(DishNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleDishNotFound(
+        DishNotFoundException ex) {
+        logger.warn(ex.getMessage());
+        Map<String, Object> response = new HashMap<>();
+        response.put("message",  ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+}
