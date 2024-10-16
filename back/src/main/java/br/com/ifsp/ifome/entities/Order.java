149488a3867a0b0ae4a4585e07920efcbf4985ed
Amanda Capistrano <amandacapistrano@outlook.com>
2024-10-15 21:14:02 -0300

    feat: new Order entity

---
+++ back/src/main/java/br/com/ifsp/ifome/entities/Order.java
@@ -0,0 +1,37 @@
+package br.com.ifsp.ifome.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @OneToMany(mappedBy = "order_id", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+    @CreationTimestamp
+    private LocalDateTime orderDate;
+    private Double orderPrice;
+    private Restaurant restaurant;
+    private DeliveryPerson deliveryPerson;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status = OrderStatus.NOVO; // Estado inicial é NOVO
+
+
+    public void calculateTotalPrice() {
+        this.orderPrice = orderItems.stream().mapToDouble(OrderItem::getTotalPrice).sum();
+    }
+
+}
