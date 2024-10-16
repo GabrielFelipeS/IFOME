38aaded50df9e6a75d2d946363fcaee4c592cf6a
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-15 22:18:01 -0300

    refactor: cleaning up repeated code

--- back/src/main/java/br/com/ifsp/ifome/services/ClientService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/ClientService.java
@@ -12,6 +12,7 @@ import br.com.ifsp.ifome.exceptions.DishNotFoundInCartException;
 import br.com.ifsp.ifome.repositories.CartRepository;
 import br.com.ifsp.ifome.repositories.ClientRepository;
 import br.com.ifsp.ifome.repositories.DishRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -30,24 +31,17 @@ public class ClientService {
     }
 
     public CartResponse getCart(String email) {
-        Optional<Client> optionalClient = clientRepository.findByEmail(email);
-        Client client = optionalClient.orElseThrow(ClientNotFoundException::new);
+        Client client = this.getClientOrElseThrow(email);
 
-        Optional<Cart> optionalCart = this.cartRepository.findFirstByClientEmail(email);
-        Cart cart = optionalCart.orElseGet(() -> new Cart(client));
+        Cart cart = getCartOrCreateNewCart(email, client);
         return new CartResponse(cart);
     }
 
     public CartResponse addDishCart(OrderRequest orderRequest, String email) {
-        Optional<Dish> optionalDish = dishRepository.findDishAvailableById(orderRequest.dishId());
-        Optional<Client> optionalClient = clientRepository.findByEmail(email);
+        Dish dish = this.getDishOrElseThrow(orderRequest.dishId());
+        Client client = this.getClientOrElseThrow(email);
 
-        Dish dish = optionalDish.orElseThrow(DishNotFoundException::new);
-        Client client = optionalClient.orElseThrow(ClientNotFoundException::new);
-
-        Optional<Cart> optionalCart = this.cartRepository.findFirstByClientEmail(email);
-        Cart cart = optionalCart.orElseGet(() -> new Cart(client));
-
+        Cart cart = this.getCartOrCreateNewCart(email, client);
         OrderItem orderItem = new OrderItem(dish, orderRequest.quantity(), cart);
         cart.add(orderItem);
 
@@ -56,35 +50,45 @@ public class ClientService {
     }
 
     public void updateQuantityOrderItemInCart(OrderRequest orderRequest, String email) {
-        Optional<Client> optionalClient = clientRepository.findByEmail(email);
-        Client client = optionalClient.orElseThrow(ClientNotFoundException::new);
+        Client client = this.getClientOrElseThrow(email);
 
-        Optional<Dish> optionalDish = dishRepository.findDishAvailableById(orderRequest.dishId());
-        Dish dish = optionalDish.orElseThrow(DishNotFoundException::new);
+        this.dishIdExistsOrElseThrow(orderRequest.dishId());
 
-        Optional<Cart> optionalCart = this.cartRepository.findFirstByClientEmail(email);
-
-        Cart cart = optionalCart.orElseGet(() -> new Cart(client));
+        Cart cart = this.getCartOrCreateNewCart(email, client);
         cart.updateDishInCart(orderRequest.dishId(), orderRequest.quantity());
         this.cartRepository.save(cart);
     }
 
-
     public void removeDishInCart(Long dishId, String email) {
-        System.err.println(dishId);
-        System.err.println("AQUI 1");
-        Optional<Client> optionalClient = clientRepository.findByEmail(email);
-        Client client = optionalClient.orElseThrow(ClientNotFoundException::new);
-        System.err.println("AQUI 2");
-        Optional<Dish> optionalDish = dishRepository.findDishAvailableById(dishId);
-        Dish dish = optionalDish.orElseThrow(DishNotFoundException::new);
-        System.err.println("AQUI 3");
+        this.dishIdExistsOrElseThrow(dishId);
+       
         Optional<Cart> optionalCart = this.cartRepository.findFirstByClientEmail(email);
-        System.err.println("AQUI 4");
         Cart cart = optionalCart.orElseThrow(DishNotFoundInCartException::new);
-        System.err.println("AQUI 5");
         cart.removeDishBy(dishId);
-        System.err.println("AQUI 6");
         this.cartRepository.save(cart);
+    }
+
+    private Cart getCartOrCreateNewCart(String email, Client client) {
+        Optional<Cart> optionalCart = this.cartRepository.findFirstByClientEmail(email);
+
+        return optionalCart.orElseGet(() -> new Cart(client));
+    }
+
+    private Client getClientOrElseThrow(String email) {
+        Optional<Client> optionalClient = clientRepository.findByEmail(email);
+        return optionalClient.orElseThrow(ClientNotFoundException::new);
+    }
+    
+    private Dish getDishOrElseThrow(Long dishId) {
+        Optional<Dish> optionalDish = dishRepository.findDishAvailableById(dishId);
+        return optionalDish.orElseThrow(DishNotFoundException::new);
+    }
+
+    private void dishIdExistsOrElseThrow(Long dishId) {
+        boolean exists = dishRepository.existsById(dishId);
+
+        if(!exists) {
+            throw new DishNotFoundException();
+        }
     }
 }
