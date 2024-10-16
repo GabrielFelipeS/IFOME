8b35b84e1dfb43f2c4e8f9ab2ba345e60e4f676d
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-15 22:04:00 -0300

    feat: getCart, update and remove dish in cart

--- back/src/main/java/br/com/ifsp/ifome/controllers/ClientController.java
+++ back/src/main/java/br/com/ifsp/ifome/controllers/ClientController.java
@@ -1,15 +1,13 @@
 package br.com.ifsp.ifome.controllers;
 
+import br.com.ifsp.ifome.docs.DocsOpenCloseRestaurant;
 import br.com.ifsp.ifome.dto.ApiResponse;
 import br.com.ifsp.ifome.dto.request.OrderRequest;
 import br.com.ifsp.ifome.dto.response.CartResponse;
 import br.com.ifsp.ifome.services.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -23,6 +21,14 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @GetMapping
+    public ResponseEntity<ApiResponse> getCart(Principal principal) {
+        CartResponse cartResponse = clientService.getCart(principal.getName());
+        ApiResponse response = new ApiResponse("success", cartResponse, "Prado adicionado no carrinho");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @PostMapping
     public ResponseEntity<ApiResponse> addDishInCart(@RequestBody OrderRequest orderRequest, Principal principal) {
         CartResponse cartResponse = clientService.addDishCart(orderRequest, principal.getName());
@@ -30,5 +36,23 @@ public class ClientController {
         ApiResponse response = new ApiResponse("success", cartResponse, "Prado adicionado no carrinho");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse> updateQuantityOrderItemInCart(@RequestBody OrderRequest orderRequest, Principal principal) {
+        clientService.updateQuantityOrderItemInCart(orderRequest, principal.getName());
+
+        ApiResponse apiResponse = new ApiResponse("success", null, "Quantidade do prato atualizado com sucesso!");
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateQuantityOrderItemInCart(@PathVariable Long id, Principal principal) {
+        clientService.removeDishInCart(id, principal.getName());
+
+        ApiResponse apiResponse = new ApiResponse("success", null, "Quantidade do prato atualizado com sucesso!");
+
+        return ResponseEntity.noContent().build();
     }
 }
