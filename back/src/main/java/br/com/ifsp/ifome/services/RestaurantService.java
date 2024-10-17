322545cae6ad7153e1c5283578c7be497c1c50f9
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-17 18:29:35 -0300

    cleanup: clear attributes

--- back/src/main/java/br/com/ifsp/ifome/services/RestaurantService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/RestaurantService.java
@@ -1,16 +1,8 @@
 package br.com.ifsp.ifome.services;
 
-import br.com.ifsp.ifome.dto.request.OrderItemRequest;
-import br.com.ifsp.ifome.dto.request.OrderRequest;
 import br.com.ifsp.ifome.dto.response.RestaurantResponse;
-import br.com.ifsp.ifome.entities.Dish;
-import br.com.ifsp.ifome.entities.Order;
-import br.com.ifsp.ifome.entities.OrderItem;
 import br.com.ifsp.ifome.entities.Restaurant;
-import br.com.ifsp.ifome.exceptions.ResourceNotFoundException;
 import br.com.ifsp.ifome.exceptions.RestaurantNotFoundException;
-import br.com.ifsp.ifome.repositories.DishRepository;
-import br.com.ifsp.ifome.repositories.OrderRepository;
 import br.com.ifsp.ifome.repositories.RestaurantRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,20 +11,15 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
-    private final DishRepository dishRepository;
-    private final OrderRepository orderRepository;
 
-    public RestaurantService(RestaurantRepository restaurantRepository, DishRepository dishRepository, OrderRepository orderRepository) {
+    public RestaurantService(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
-        this.dishRepository = dishRepository;
-        this.orderRepository = orderRepository;
     }
 
     public String changeStateOpen(Principal principal) {
