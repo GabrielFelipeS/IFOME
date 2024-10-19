96af06332c62eacff4b0cedced717531512f418d
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-19 11:42:46 -0300

    fix: query in cart repository

--- back/src/main/java/br/com/ifsp/ifome/repositories/CartRepository.java
+++ back/src/main/java/br/com/ifsp/ifome/repositories/CartRepository.java
@@ -1,10 +1,13 @@
 package br.com.ifsp.ifome.repositories;
 
 import br.com.ifsp.ifome.entities.Cart;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CartRepository extends CrudRepository<Cart, Long> {
-    Optional<Cart> findFirstByClientEmail(String email);
+    @Query("SELECT c FROM Cart c JOIN c.orderItems o WHERE c.client.email = :email AND c.customerOrder IS NULL")
+    Optional<Cart> findFirstByClientEmail(@Param("email") String email);
 }
