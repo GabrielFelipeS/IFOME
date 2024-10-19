bf750df27c2615d8189ddb88ea0e7ee5993d2067
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-19 14:24:11 -0300

    cleanup: CustomerOrderController

--- back/src/main/java/br/com/ifsp/ifome/controllers/CustomerOrderController.java
+++ back/src/main/java/br/com/ifsp/ifome/controllers/CustomerOrderController.java
@@ -1,39 +1,30 @@
 package br.com.ifsp.ifome.controllers;
 
 import br.com.ifsp.ifome.dto.ApiResponse;
-import br.com.ifsp.ifome.dto.request.OrderRequest;
-import br.com.ifsp.ifome.entities.Cart;
-import br.com.ifsp.ifome.entities.Client;
-import br.com.ifsp.ifome.repositories.CartRepository;
+import br.com.ifsp.ifome.dto.response.CustomerOrderRequest;
 import br.com.ifsp.ifome.services.CustomerOrderService;
-import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/order")
 public class CustomerOrderController {
 
     private final CustomerOrderService customerOrderService;
-    private final CartRepository cartRepository;
 
-    public CustomerOrderController(CustomerOrderService customerOrderService, CartRepository cartRepository) {
+    public CustomerOrderController(CustomerOrderService customerOrderService) {
         this.customerOrderService = customerOrderService;
-        this.cartRepository = cartRepository;
     }
 
-    @PostMapping("/{id}/order")
+    @PostMapping
     public ResponseEntity<ApiResponse> createOrder(
         Principal principal) {
-        String custumerEmail = principal.getName();
-        Optional<Cart> cartCustumer =  cartRepository.findFirstByClientEmail(custumerEmail);
-        // Chamar o serviço de criação do pedido
-        String message = customerOrderService.createOrder(principal);
+        CustomerOrderRequest customerOrderRequest = customerOrderService.createOrder(principal);
 
-        ApiResponse apiResponse = new ApiResponse("success", null, message);
-        return ResponseEntity.ok(apiResponse);
+        ApiResponse apiResponse = new ApiResponse("success", customerOrderRequest, "Pedido enviado! Aguarde confimação!");
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
     }
 }
