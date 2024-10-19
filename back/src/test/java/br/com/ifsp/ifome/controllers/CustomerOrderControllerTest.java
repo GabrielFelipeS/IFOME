39c8ba5b13328855ece944933f2c3acb5feee1a2
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-19 15:24:34 -0300

    wip: CustomerOrderControllerTest

---
+++ back/src/test/java/br/com/ifsp/ifome/controllers/CustomerOrderControllerTest.java
@@ -0,0 +1,67 @@
+package br.com.ifsp.ifome.controllers;
+
+import br.com.ifsp.ifome.services.CustomerOrderService;
+import br.com.ifsp.ifome.services.TokenService;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerOrderControllerTest {
+
+    @Autowired
+    private TokenService tokenService;
+
+    private String token_cliente;
+
+    private String token_cliente_with_customer_order;
+
+    private MockMvc mockMvc;
+
+    CustomerOrderService customerOrderService;
+
+    @BeforeEach
+    public void setUp() {
+        this.token_cliente_with_customer_order = tokenService.generateToken("user1@gmail.com",  List.of(new SimpleGrantedAuthority("ROLE_CLIENT")));
+        this.token_cliente = tokenService.generateToken("email1@email.com",  List.of(new SimpleGrantedAuthority("ROLE_CLIENT")));
+
+        customerOrderService = Mockito.spy(customerOrderService);
+
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(new CustomerOrderController(customerOrderService))
+            .build();
+    }
+
+
+    @Test
+    public void deveReceberSseEvent() throws Exception {
+        mockMvc.perform(get("/api/order/status/")
+            .header(HttpHeaders.AUTHORIZATION,  "Bearer " + token_cliente_with_customer_order))
+            .andExpect(status().isOk());
+    }
+
+    private @NotNull HttpHeaders getHttpHeadersClient() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token_cliente);
+        return headers;
+    }
+
+
+
+}
