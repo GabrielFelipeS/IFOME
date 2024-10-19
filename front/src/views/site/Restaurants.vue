d856458782937cebc67cf8f745fc604b6d23bbdd
Micael Hernandes <micael.hernandes>
2024-10-19 13:02:52 -0300

    feat: Add FooterMobile component to Restaurants view

--- front/src/views/site/Restaurants.vue
+++ front/src/views/site/Restaurants.vue
@@ -2,10 +2,13 @@
 
 <Header />
 
+<FooterMobile />
+
 </template>
 
 <script setup>
 
 import Header from '@/components/site/Header.vue'
+import FooterMobile from '@/components/site/FooterMobile.vue'
 
 </script>
