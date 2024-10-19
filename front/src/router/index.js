1b6116b25a903da437d59f783b2ba9bcd4bb1021
Micael Hernandes <micael.hernandes>
2024-10-19 11:45:02 -0300

    feat: create route to show restaurants

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      children:[
+        {
+          path: '/restaurants',
+          name: 'restaurants',
+          component: () => import('@/views/site/Restaurants.vue'),
+        }
+      ]
+    },
+    {
       path: '/store',
       children: [
         {
