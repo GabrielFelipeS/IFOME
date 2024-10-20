5ac3af4f96d0e8133fb54f25957f0676dbc4a8df
Micael Hernandes <micael.hernandes>
2024-10-20 14:28:13 -0300

    refactor: Update Restaurants.vue to dynamically display restaurants from API

--- front/src/views/site/Restaurants.vue
+++ front/src/views/site/Restaurants.vue
@@ -6,45 +6,18 @@
             <h2 class="text-2xl mt-2 ml-2 font-normal text-tertiary-light">Restaurantes</h2>
 
             <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 mt-5 mb-[100px]">
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
-                <CardRestaurant />
+                <template v-if="restaurants.length > 0">
+
+                    <CardRestaurant
+                        v-for="(restaurant, index) in restaurants"
+                        :key="restaurant.id || index"
+                        :restaurant="restaurant"
+                    />
+                </template>
+
+                <template v-else>
+                    <p class="col-span-full text-center">Nenhum restaurante encontrado</p>
+                </template>
             </div>
         </div>
     </main>
@@ -52,8 +25,22 @@
 </template>
 
 <script setup>
-import Header from '@/components/site/Header.vue'
-import FooterMobile from '@/components/site/FooterMobile.vue'
-import DropFilterArea from '@/components/site/DropFilterArea.vue'
-import CardRestaurant from '@/components/site/CardRestaurant.vue'
+import Header from '@/components/site/Header.vue';
+import FooterMobile from '@/components/site/FooterMobile.vue';
+import DropFilterArea from '@/components/site/DropFilterArea.vue';
+import CardRestaurant from '@/components/site/CardRestaurant.vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const restaurants = ref([]);
+
+onMounted(async () => {
+    try {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}restaurant/`);
+        restaurants.value = data.data.content || []; 
+    } catch (error) {
+        console.error("Erro ao buscar os restaurantes:", error);
+        restaurants.value = []; 
+    }
+});
 </script>
