5602f8202c9d2b5284c96dbf3ac56816ecbeb5a6
Micael Hernandes <micael.hernandes>
2024-10-20 16:23:58 -0300

    refactor: Remove DropFilterArea component from Restaurants.vue

--- front/src/views/site/Restaurants.vue
+++ front/src/views/site/Restaurants.vue
@@ -2,7 +2,6 @@
     <Header />
     <main class="w-full min-h-[calc(100vh-75px)] bg-white md:mt-[75px]">
         <div class="w-[90%] md:max-w-[1200px] mx-auto flex flex-col md:p-5">
-            <DropFilterArea />
             <h2 class="text-2xl mt-2 ml-2 font-normal text-tertiary-light">Restaurantes</h2>
 
             <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 mt-5 mb-[100px]">
