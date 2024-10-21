2d4c118cda975b8dabd7f61e422fb43de66fc953
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-20 22:19:18 -0300

    feat: async config

---
+++ back/src/main/java/br/com/ifsp/ifome/infra/AsyncConfig.java
@@ -0,0 +1,10 @@
+package br.com.ifsp.ifome.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
+
