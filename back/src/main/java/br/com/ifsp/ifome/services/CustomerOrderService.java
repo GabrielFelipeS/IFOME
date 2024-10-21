dacafffada4b3cf5430258ad2665a4a1c683d672
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-21 13:39:09 -0300

    feat: emitter methods

--- back/src/main/java/br/com/ifsp/ifome/services/CustomerOrderService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/CustomerOrderService.java
@@ -10,16 +10,21 @@ import br.com.ifsp.ifome.events.PedidoStatusChangedEvent;
 import br.com.ifsp.ifome.exceptions.CartCannotBeEmptyException;
 import br.com.ifsp.ifome.exceptions.RestaurantNotFoundException;
 import br.com.ifsp.ifome.repositories.CartRepository;
-import br.com.ifsp.ifome.repositories.DishRepository;
 import br.com.ifsp.ifome.repositories.CustomerOrderRepository;
+import br.com.ifsp.ifome.repositories.DishRepository;
 import br.com.ifsp.ifome.repositories.RestaurantRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.yaml.snakeyaml.emitter.EmitterException;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,6 +34,7 @@ public class CustomerOrderService {
     private final CustomerOrderRepository customerOrderRepository;
     private final CartRepository cartRepository;
     private final ApplicationEventPublisher eventPublisher;
+    private final Map<Long, SseEmitter> sseEmitterHashMap;
 
     public CustomerOrderService(RestaurantRepository restaurantRepository, DishRepository dishRepository, CustomerOrderRepository customerOrderRepository, CartRepository cartRepository, ApplicationEventPublisher eventPublisher) {
         this.restaurantRepository = restaurantRepository;
@@ -36,21 +42,24 @@ public class CustomerOrderService {
         this.customerOrderRepository = customerOrderRepository;
         this.cartRepository = cartRepository;
         this.eventPublisher = eventPublisher;
+        this.sseEmitterHashMap = new ConcurrentHashMap<>();
     }
 
     public CustomerOrderRequest createOrder(Principal principal) {
         Cart cart = cartRepository
-                        .findFirstByClientEmail(principal.getName())
-                        .orElseThrow(CartCannotBeEmptyException::new)
-                        .cartCannotBeEmpty();
+            .findFirstByClientEmail(principal.getName())
+            .orElseThrow(CartCannotBeEmptyException::new)
+            .cartCannotBeEmpty();
 
-       Restaurant restaurant = restaurantRepository
-                                .findById(cart.getIdRestaurant())
-                                .orElseThrow(RestaurantNotFoundException::new);
+        Restaurant restaurant = restaurantRepository
+            .findById(cart.getIdRestaurant())
+            .orElseThrow(RestaurantNotFoundException::new);
 
         CustomerOrder customerOrder = new CustomerOrder(cart, restaurant);
 
         customerOrderRepository.save(customerOrder);
+
+        this.addEmitter(customerOrder);
 
         return CustomerOrderRequest.from(customerOrder);
     }
@@ -85,7 +94,6 @@ public class CustomerOrderService {
         CustomerOrder customerOrder = customerOrderRepository.findById(orderId)
             .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado com ID: " + orderId));
 
-
         OrderStatus currentStatus = customerOrder.getStatus();
         OrderStatus nextStatus = getNextStatus(currentStatus);
 
@@ -93,6 +101,7 @@ public class CustomerOrderService {
         customerOrder.setStatus(nextStatus);
         customerOrderRepository.save(customerOrder);
 
+        this.updateStatusEmitter(customerOrder);
         eventPublisher.publishEvent(new PedidoStatusChangedEvent(orderId, nextStatus, customerOrder));
     }
 
@@ -113,6 +122,59 @@ public class CustomerOrderService {
 
         // Retorna o próximo status na sequência
         return validSequence.get(currentIndex + 1);
+    }
+
+    public SseEmitter getEmitter(Long id) {
+        System.err.println(id);
+        Optional<CustomerOrder> customerOrder = customerOrderRepository.findById(id);
+        SseEmitter emitter =  this.getEmitter(customerOrder.get());
+        System.err.println(emitter);
+        return emitter;
+    }
+
+    public SseEmitter getEmitter(CustomerOrder customerOrder) {
+        if(sseEmitterHashMap.containsKey(customerOrder.getId())) {
+            return sseEmitterHashMap.get(customerOrder.getId());
+        }
+
+        return this.addEmitter(customerOrder);
+    }
+
+
+    private SseEmitter addEmitter(CustomerOrder customerOrder) {
+        System.err.println("CRIANDO EMITTER");
+        SseEmitter emitter = new SseEmitter(3_600_000L );
+
+        System.err.println("ONTIMEOUT EMITTER");
+        emitter.onTimeout(() -> sseEmitterHashMap.remove(customerOrder.getId()));
+
+        System.err.println("ONCOMPLETION EMITTER");
+        emitter.onCompletion(() -> {
+            sseEmitterHashMap.remove(customerOrder.getId());
+            System.err.println("EXECUTOU E NÂO DEVIA");
+            emitter.complete();
+        });
+
+        System.err.println("ONERROR EMITTER");
+        emitter.onError((e) -> System.err.println("Emmiter Erro: " + e.getMessage()));
+
+        System.err.println("ADICIONANDO EMITTER");
+        sseEmitterHashMap.put(customerOrder.getId(), emitter);
+        System.err.println(customerOrder.getId());
+        return emitter;
+    }
+
+    private void updateStatusEmitter(CustomerOrder customerOrder)  {
+        System.err.println("PEGANDO EMITTER");
+        SseEmitter emitter = this.getEmitter(customerOrder);
+        System.err.println(emitter);
+        try {
+            System.err.println("ANTES DE ENVIAR MENSAGEM EMITTER");
+            emitter.send(customerOrder.getStatusMessage());
+            System.err.println("DEPOIS DE ENVIAR MENSAGEM EMITTER");
+        } catch (IOException e) {
+            throw new EmitterException("Emmiter erro: " + e.getMessage());
+        }
     }
 
 }
