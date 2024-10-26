3c6f3a9736326dab2761ab07f2ba4643991ea0da
Micael Hernandes <micael.hernandes>
2024-10-26 17:36:34 -0300

    feat: Add CardOrder component to store panel

---
+++ front/src/components/store/panel/CardOrder.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div
+        class="w-full h-[100px] border border-soft-border flex flex-row justify-around items-center text-tertiary-light">
+        <div>
+            <p class="font-bold">#1234</p>
+            <p>19:30</p>
+        </div>
+        <div>
+            <p class="font-bold">MICAEL</p>
+            <p>Previsão: 20:15</p>
+        </div>
+        <div>
+            <p class="bg-red-500 text-white px-5 py-2 rounded-full">Em Preparo</p>
+        </div>
+    </div>
+
+</template>
