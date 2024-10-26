8ac771df1bdf97518dbdfbb232c2d5b48cfc0bef
Micael Hernandes <micael.hernandes>
2024-10-26 17:36:43 -0300

    refactor: Update FilterOrders component to include height styling

--- front/src/components/store/panel/FilterOrders.vue
+++ front/src/components/store/panel/FilterOrders.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex flex-row justify-center">
+    <div class="w-full h-[80px] flex flex-row justify-center">
         <select name="" id=""
             class="w-[95%] p-3 outline-none bg-white border border-soft-border rounded-md my-3 px-4 appearance-none pr-8"
             style="background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij4KICA8cGF0aCBkPSJNNC4wNjcgNi42NjdsNC4yOTQgNC4yOTMgNC4yOTQtNC4yOTMuNzA4LjcwOC00Ljk5NyA0Ljk5Ny00Ljk5Ny00Ljk5Ny43MDgtLjcwOHoiLz4KPC9zdmc+'); /* Ícone da seta */
