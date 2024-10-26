4880c2bf23dbf5aaed62e1eace44cc62236a4933
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-26 19:51:43 -0300

    fix: image get

--- back/src/main/java/br/com/ifsp/ifome/infra/WebConfig.java
+++ back/src/main/java/br/com/ifsp/ifome/infra/WebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String PATH_TO_FOLDER_IMAGES = System.getProperty("user.dir") + "/src/main/resources/static/images/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/image/**")
-            .addResourceLocations("classpath:/static/images/");
+            .addResourceLocations("file:" + PATH_TO_FOLDER_IMAGES);
     }
 }
