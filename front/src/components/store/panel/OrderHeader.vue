df004e58cb9d94a6c06fbb5b624f6dcc2ebbcc32
Micael Hernandes <micael.hernandes>
2024-10-26 19:18:47 -0300

    feat: Add OrderHeader component to store panel

---
+++ front/src/components/store/panel/OrderHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="w-full grid grid-cols-3 h-[20vh] border-b ">
+        <div class="w-full h-full flex flex-col justify-center items-start pl-5 text-tertiary-light gap-3">
+            <div>
+                <p class="font-bold">PEDIDO</p>
+                <p>#1020</p>
+            </div>
+            <div>
+                <p class="font-bold">CEP</p>
+                <p>12345-678</p>
+            </div>
+            <div>
+                <p class="font-bold">COMPLEMENTO</p>
+                <p>AP 123</p>
+            </div>
+        </div>
+        <div class="w-full h-full flex flex-col justify-center items-start pl-5 text-tertiary-light gap-3">
+            <div>
+                <p class="font-bold">CLIENTE</p>
+                <p>Micael</p>
+            </div>
+            <div>
+                <p class="font-bold">ENDEREÇO</p>
+                <p>Rua dos Bobos, 0</p>
+            </div>
+            <div>
+                <p class="font-bold">REFERÊNCIA</p>
+                <p>Próximo ao bar do Zé</p>
+            </div>
+        </div>
+        <div class="w-full h-full flex flex-col justify-center items-center pl-5 text-tertiary-light gap-3">
+            <p class="bg-green-500 text-white px-10 rounded py-2">Finalizar</p>
+            <span>Previsão: 19:00</span>
+        </div>
+    </div>
+</template>
