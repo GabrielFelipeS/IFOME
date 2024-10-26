cfc70f8b19b0ffea545dfeb32ac4d00d5864629b
Micael Hernandes <micael.hernandes>
2024-10-26 17:36:53 -0300

    feat: Add Orders component to store panel

---
+++ front/src/components/store/panel/Orders.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-[450px] h-[100vh] border-r border-soft-border">
+        <FilterOrders @filter="ordemProducts" />
+        <div class="w-full h-[calc(100vh_-_80px)] overflow-y-auto overflow-x-hidden">
+            <CardOrder />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import CardOrder from './CardOrder.vue';
+import FilterOrders from './FilterOrders.vue';
+
+function ordemProducts(value) {
+    console.log(value);
+}
+
+</script>
