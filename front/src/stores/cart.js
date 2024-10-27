f750d35a45aec932ba7a96a4bc736f2da0e84fca
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-27 18:26:00 -0300

    feat: add cart store

---
+++ front/src/stores/cart.js
@@ -0,0 +1,14 @@
+import {defineStore} from "pinia";
+import {computed, ref} from "vue";
+
+export const useCart = defineStore("cart", () => {
+	const totalPrice = ref(0);
+	const totalItems = ref(0);
+
+	function updateCart(order) {
+		totalPrice.value = order.totalPrice;
+		totalItems.value = order.quantity;
+	}
+
+	return {totalPrice, totalItems, updateCart};
+})
