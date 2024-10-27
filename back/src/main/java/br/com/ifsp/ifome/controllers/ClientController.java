1c6c29c956defe8201e726355ad50633997ab792
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-27 20:45:50 -0300

    refactor: CustomerOrderController endpoints to ClientController endpoints

--- back/src/main/java/br/com/ifsp/ifome/controllers/ClientController.java
+++ back/src/main/java/br/com/ifsp/ifome/controllers/ClientController.java
@@ -1,30 +1,56 @@
 package br.com.ifsp.ifome.controllers;
 
-import br.com.ifsp.ifome.docs.DocsDeleteDishInCart;
-import br.com.ifsp.ifome.docs.DocsGetCart;
-import br.com.ifsp.ifome.docs.DocsInsertOrderItemInCart;
-import br.com.ifsp.ifome.docs.DocsUpdateItemInCart;
+import br.com.ifsp.ifome.docs.*;
 import br.com.ifsp.ifome.dto.ApiResponse;
 import br.com.ifsp.ifome.dto.request.OrderItemRequest;
 import br.com.ifsp.ifome.dto.request.OrderItemUpdateRequest;
 import br.com.ifsp.ifome.dto.response.CartResponse;
+import br.com.ifsp.ifome.dto.response.CustomerOrderRequest;
+import br.com.ifsp.ifome.dto.response.CustomerOrderResponse;
 import br.com.ifsp.ifome.services.ClientService;
+import br.com.ifsp.ifome.services.CustomerOrderService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
 
     private final ClientService clientService;
+    private final CustomerOrderService customerOrderService;
 
-    public ClientController(ClientService clientService) {
+    public ClientController(ClientService clientService, CustomerOrderService customerOrderService) {
         this.clientService = clientService;
+        this.customerOrderService = customerOrderService;
     }
+
+    @PostMapping("/order/")
+    @DocsCreateCustomerOrder
+    public ResponseEntity<ApiResponse> createOrder(
+        Principal principal) {
+        CustomerOrderRequest customerOrderRequest = customerOrderService.createOrder(principal);
+
+        ApiResponse apiResponse = new ApiResponse("success", customerOrderRequest, "Pedido enviado! Aguarde confimação!");
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @GetMapping("/order/")
+    @DocGetCustomerOrders
+    public ResponseEntity<List<CustomerOrderResponse>> getAllCustomerOrders(Principal principal) {
+        List<CustomerOrderResponse> orders = customerOrderService.getAllOrdersByCustomer(principal.getName());
+
+        if (orders.isEmpty()) {
+            return ResponseEntity.noContent().build(); // Return 204 No Content if no orders found
+        }
+
+        return ResponseEntity.ok(orders); // Return 200 OK with the list of orders
+    }
+
 
     @GetMapping("/cart")
     @DocsGetCart
