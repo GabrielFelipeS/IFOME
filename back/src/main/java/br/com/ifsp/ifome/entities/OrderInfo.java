ffb6486d121e310e479b27f201b3d9a633679ce7
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-27 18:28:50 -0300

    feat: OrderInfo

---
+++ back/src/main/java/br/com/ifsp/ifome/entities/OrderInfo.java
@@ -0,0 +1,29 @@
+package br.com.ifsp.ifome.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    private LocalDateTime localDateTime;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_order")
+    private CustomerOrder customerOrder;
+}
