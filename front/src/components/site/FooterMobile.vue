2ea1400f1325215291d30e19c7d96b374908e33b
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-27 22:30:16 -0300

    wip: add "see-bag" on FooterMobile

--- front/src/components/site/FooterMobile.vue
+++ front/src/components/site/FooterMobile.vue
@@ -1,9 +1,14 @@
 <template>
-    <footer
-        class="w-full h-[75px] bg-white flex flex-row items-center justify-around fixed bottom-0 left-0 shadow-black shadow-2xl md:hidden z-50">
-        <v-icon name="fa-home" scale="1.5" class="text-primary cursor-pointer" />
-        <v-icon name="fa-shopping-bag" scale="1.5" class="text-primary cursor-pointer" @click="$emit('open-cart')"/>
-        <v-icon name="fa-comment-alt" scale="1.5" class="text-primary cursor-pointer" />
-        <v-icon name="fa-sign-out-alt" scale="1.5" class="text-primary cursor-pointer" />
-    </footer>
+	<div class="fixed bottom-[80px] w-full px-8 py-2 flex flex-row justify-between z-50 bg-white">
+		<span>10,00</span>
+		<button>Ver sacola <v-icon name="fa-chevron-right" /></button>
+	</div>
+	<footer
+		class="w-full h-[75px] bg-white flex flex-row items-center justify-around fixed bottom-0 left-0 shadow-black shadow-2xl md:hidden z-50">
+		<v-icon name="fa-home" scale="1.5" class="text-primary cursor-pointer" />
+		<v-icon name="fa-shopping-bag" scale="1.5" class="text-primary cursor-pointer" @click="$emit('open-cart')"/>
+		<v-icon name="fa-comment-alt" scale="1.5" class="text-primary cursor-pointer" />
+		<v-icon name="fa-sign-out-alt" scale="1.5" class="text-primary cursor-pointer" />
+	</footer>
 </template>
+
