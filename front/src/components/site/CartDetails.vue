88d5c024685194125f21cf9b8790b89144e10422
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-10-27 21:17:22 -0300

    style: adjust responsiveness

--- front/src/components/site/CartDetails.vue
+++ front/src/components/site/CartDetails.vue
@@ -1,8 +1,10 @@
 <script setup>
 import {useCart} from "@/stores/cart.js";
+import {ref} from "vue";
 
 const cart = useCart();
 const order = cart.order;
+const deliveryFee = ref(0);
 
 console.log(order)
 
@@ -10,7 +12,7 @@ console.log(order)
 
 <template>
 	<div class="main">
-		<div class="restaurant-description">
+		<div class="restaurant-description my-8">
 			<div class="flex flex-col text-center gap-3">
 				<span class="text-tertiary-light text-xs">Seu pedido em</span>
 				<span class="font-semibold">Antojitos</span>
@@ -19,8 +21,26 @@ console.log(order)
 				Ver cardápio
 			</button>
 		</div>
-		<div class="order-items">
-			<span class="font-semibold mb-4">Resumo do pedido:</span>
+		<span class="font-semibold mt-8">Resumo do pedido:</span>
+		<div class="order-items mb-8">
+			<div class="product" v-for="item in order.orderItems">
+				<div class="flex flex-col items-start gap-1.5">
+					<span>{{ item.dish.name }}</span>
+					<button class="text-primary text-xs font-semibold">Remover</button>
+				</div>
+				<div class="mr-6">
+					R$ {{ item.totalPrice.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2}) }}
+				</div>
+			</div>
+			<div class="product" v-for="item in order.orderItems">
+				<div class="flex flex-col items-start gap-1.5">
+					<span>{{ item.dish.name }}</span>
+					<button class="text-primary text-xs font-semibold">Remover</button>
+				</div>
+				<div class="mr-6">
+					R$ {{ item.totalPrice.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2}) }}
+				</div>
+			</div>
 			<div class="product" v-for="item in order.orderItems">
 				<div class="flex flex-col items-start gap-1.5">
 					<span>{{ item.dish.name }}</span>
@@ -31,21 +51,21 @@ console.log(order)
 				</div>
 			</div>
 		</div>
-		<div class="order-summary">
+		<div class="order-summary my-8">
 			<div class="flex flex-row justify-between text-xs text-tertiary-light">
 				<span>Subtotal</span>
-				<span>R$ 10,00</span>
+				<span>R$ {{ order.totalPrice.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2}) }}</span>
 			</div>
 			<div class="flex flex-row justify-between text-xs text-tertiary-light">
 				<span>Taxa de entrega</span>
-				<span>R$ 10,00</span>
+				<span>R$ {{ deliveryFee.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2}) }}</span>
 			</div>
 			<div class="flex flex-row justify-between font-semibold">
 				<span>Total</span>
-				<span>R$ 10,00</span>
+				<span>R$ {{ (order.totalPrice + deliveryFee).toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2}) }}</span>
 			</div>
 		</div>
-		<div class="flex flex-row justify-center h-full">
+		<div class="flex flex-row justify-center h-full my-8">
 			<button class="p-2 bg-primary text-white/80 rounded-md h-fit self-end">
 				Escolher formas de pagamento <v-icon name="fa-chevron-right" />
 			</button>
@@ -57,19 +77,19 @@ console.log(order)
 	.main {
 		@apply bg-white fixed right-0 bottom-0 z-40 rounded-xl;
 		@apply w-full max-w-[600px] h-full max-h-[95%] p-4 pb-[116px];
-		@apply flex flex-col gap-16 overflow-y-scroll;
+		@apply flex flex-col overflow-y-scroll no-scrollbar;
 	}
 	.restaurant-description {
-		@apply flex flex-row justify-between mx-12 mt-6;
+		@apply flex flex-row justify-between mx-12 mt-6 max-h-[20%];
 	}
 	.order-items {
-		@apply flex flex-col;
+		@apply flex flex-col min-h-[40%] overflow-y-scroll;
 	}
 	.product {
-		@apply flex flex-row justify-between mx-2 py-4;
+		@apply flex flex-row justify-between mx-2 py-4 -mt-1;
 		@apply border-t-4 border-b-4 border-soft-border;
 	}
 	.order-summary {
-		@apply flex flex-col gap-4 mx-12 font-semibold;
+		@apply flex flex-col gap-4 mx-12 font-semibold max-h-[20%];
 	}
 </style>
