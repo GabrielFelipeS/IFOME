bf5bf03c209c028d0bd50c8c01391cb9008ddffd
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-10-28 23:34:54 -0300

    feat: new method findAllByDeliveryPerson in CustomerOrderRepository

--- back/src/main/java/br/com/ifsp/ifome/repositories/CustomerOrderRepository.java
+++ back/src/main/java/br/com/ifsp/ifome/repositories/CustomerOrderRepository.java
@@ -2,6 +2,8 @@ package br.com.ifsp.ifome.repositories;
 
 import br.com.ifsp.ifome.entities.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,7 @@ public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Lo
     List<CustomerOrder> findAllByCartClientEmail(String email);
 
     List<CustomerOrder> findByRestaurantId(Long restaurantId);
+
+    @Query("SELECT c FROM CustomerOrder c WHERE c.deliveryPerson.email = :email AND c.paymentStatus != 'PAGO'")
+    List<CustomerOrder> findAllByDeliveryPerson(@Param("email") String email);
 }
