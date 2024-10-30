99174f8501d3491cd45a097958bc57a821b84112
Internautilo <yuribertulucci2004@gmail.com>
2024-10-29 21:44:27 -0300

    feat: update cart via api if no param sent

--- front/src/stores/cart.js
+++ front/src/stores/cart.js
@@ -1,12 +1,24 @@
 import {defineStore} from "pinia";
 import {ref} from "vue";
+import api from "@/services/api.js";
 
 export const useCart = defineStore("cart", () => {
 	const totalPrice = ref(0);
 	const totalItems = ref(0);
 	const order = ref({});
 
-	function updateCart(param) {
+	async function updateCart(param = null) {
+		if (param == null && localStorage.getItem('token') !== null) {
+			try {
+				const response = await api.get('client/cart');
+				param = response.data.data;
+			} catch (e) {
+				console.log("Erro ao carregar carrinho no Pinia: " + e);
+			}
+		}
+		if (param === null) {
+			throw new Error("Erro no carregamento do carrinho por api (Pinia)");
+		}
 		totalPrice.value = param.totalPrice;
 		totalItems.value = param.totalQuantity;
 		order.value = param;
