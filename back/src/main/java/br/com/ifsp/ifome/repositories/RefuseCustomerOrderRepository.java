353263d47b85e7eb2e67988dd02d3ad7790a0d01
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-02 22:34:37 -0300

    refactor: new entity

---
+++ back/src/main/java/br/com/ifsp/ifome/repositories/RefuseCustomerOrderRepository.java
@@ -0,0 +1,7 @@
+package br.com.ifsp.ifome.repositories;
+
+import br.com.ifsp.ifome.entities.RefuseCustomerOrder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefuseCustomerOrderRepository extends CrudRepository<RefuseCustomerOrder, Long> {
+}
