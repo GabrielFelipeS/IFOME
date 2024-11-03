e06b17a8822c81ff9fd8db7a2b697817770325a7
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-02 22:36:05 -0300

    feat: EnableSpringDataWebSupport

--- back/src/main/java/br/com/ifsp/ifome/IfomeApplication.java
+++ back/src/main/java/br/com/ifsp/ifome/IfomeApplication.java
@@ -3,8 +3,12 @@ package br.com.ifsp.ifome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @EnableAspectJAutoProxy
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class IfomeApplication {
 
