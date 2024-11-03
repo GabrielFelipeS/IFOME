255f035c3acd8ad7434c9977c4f245063304bb9c
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-02 22:27:58 -0300

    feat: DeliveryPersonExceptionHandler

---
+++ back/src/main/java/br/com/ifsp/ifome/exceptions/DeliveryPersonExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.ifsp.ifome.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DeliveryPersonExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(DeliveryPersonExceptionHandler.class);
+
+    @ExceptionHandler(DeliveryPersontNotFoundException.class)
+    public  ResponseEntity<Map<String, Object>>  handleMaxUploadSizeExceededPart(
+        DeliveryPersontNotFoundException ex) {
+        logger.warn(ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message",  ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+}
