c6c08f44371a187bc8b414b7959b9778a06bf6f2
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-04 10:18:36 -0300

    fix: OrderDeliveryStatus

--- back/src/main/java/br/com/ifsp/ifome/entities/OrderDeliveryStatus.java
+++ back/src/main/java/br/com/ifsp/ifome/entities/OrderDeliveryStatus.java
@@ -3,8 +3,8 @@ package br.com.ifsp.ifome.entities;
 public enum OrderDeliveryStatus {
     NOVO,
     ACEITO,
-    NO_LOCAL,
-    EM_PREPARO,
+//    NO_LOCAL,
+//    EM_PREPARO,
     PRONTO,
     A_CAMINHO,
     CONCLUIDO
