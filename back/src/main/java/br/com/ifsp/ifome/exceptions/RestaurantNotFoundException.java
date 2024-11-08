0d1b97d521dc6dcb153bf6d2af63fd8d38b26ac9
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-07 22:20:14 -0300

    feat: constructor with message

--- back/src/main/java/br/com/ifsp/ifome/exceptions/RestaurantNotFoundException.java
+++ back/src/main/java/br/com/ifsp/ifome/exceptions/RestaurantNotFoundException.java
@@ -1,6 +1,10 @@
 package br.com.ifsp.ifome.exceptions;
 
 public class RestaurantNotFoundException extends RuntimeException{
+    public RestaurantNotFoundException(String message) {
+        super(message);
+    }
+
     public RestaurantNotFoundException() {
         super("Restaurante não encontrado");
     }
