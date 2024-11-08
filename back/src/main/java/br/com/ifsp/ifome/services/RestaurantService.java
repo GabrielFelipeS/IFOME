43c38eed7cc7210341c4a29faba448d36dcd9505
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-07 22:21:44 -0300

    feat: JavaDoc and getOrders method

--- back/src/main/java/br/com/ifsp/ifome/services/RestaurantService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/RestaurantService.java
@@ -1,8 +1,11 @@
 package br.com.ifsp.ifome.services;
 
+import br.com.ifsp.ifome.dto.response.CustomerOrderResponse;
 import br.com.ifsp.ifome.dto.response.RestaurantResponse;
+import br.com.ifsp.ifome.entities.CustomerOrder;
 import br.com.ifsp.ifome.entities.Restaurant;
 import br.com.ifsp.ifome.exceptions.RestaurantNotFoundException;
+import br.com.ifsp.ifome.repositories.CustomerOrderRepository;
 import br.com.ifsp.ifome.repositories.RestaurantRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,20 +13,28 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
+    private final CustomerOrderRepository customerOrderRepository;
 
-    public RestaurantService(RestaurantRepository restaurantRepository) {
+    public RestaurantService(RestaurantRepository restaurantRepository, CustomerOrderRepository customerOrderRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.customerOrderRepository = customerOrderRepository;
     }
 
-    public String changeStateOpen(Principal principal) {
-        var restaurant = restaurantRepository.findByEmail(principal.getName()).get();
+    /**
+     * Abre/fecha o restaurante baseado no status atual dele
+     *
+     * @param email Email do restaurante logado
+     * @return retorna a mensagem de atualização do restaurante
+     * @throws RestaurantNotFoundException Caso não encontre o restaurante com o id especificado
+     */
+    public String changeStateOpen(String email) {
+        var restaurant = restaurantRepository.findByEmail(email).orElseThrow(RestaurantNotFoundException::new);
         restaurant.reverseStatusOpen();
         restaurantRepository.save(restaurant);
 
@@ -31,10 +42,26 @@ public class RestaurantService {
         return message;
     }
 
+    /**
+     * Retorna a lista de restaurantes em ordem alfabetica
+     *
+     * @return lista de restaurantes
+     */
     public List<RestaurantResponse> getAllRestaurants() {
-        return restaurantRepository.findAll(Sort.by(Sort.Direction.ASC, "nameRestaurant")).stream().map(RestaurantResponse::from).collect(Collectors.toList());
+        return restaurantRepository.findAll(
+            Sort.by(
+                Sort.Direction.ASC, "nameRestaurant")
+            ).stream()
+            .map(RestaurantResponse::from)
+            .toList();
     }
 
+    /**
+     * Retorna a lista de restaurantes em ordem alfabetica
+     *
+     * @param pageable informações de paginação
+     * @return lista de restaurantes
+     */
      public Page<RestaurantResponse> getAllRestaurants(Pageable pageable) {
         return restaurantRepository.findAll(PageRequest.of(
                     pageable.getPageNumber(),
@@ -43,10 +70,37 @@ public class RestaurantService {
                 )).map(RestaurantResponse::from);
     }
 
+    /**
+     * Busca um restaurante pelo id
+     *
+     * @param id id do restaurante
+     * @return restaurante com id passado como parametro
+     * @throws RestaurantNotFoundException Caso não encontre o restaurante com o id especificado
+     */
     public RestaurantResponse findById(Long id) {
         Restaurant restaurant = restaurantRepository.findById(id)
             .orElseThrow(RestaurantNotFoundException::new);
         return RestaurantResponse.from(restaurant);
     }
 
+    /**
+     * Busca todos os pedidos de um restaurante
+     *
+     * @param email E-mail do restaurante logado
+     * @return Lista de pedidos do restaurante
+     * @throws RestaurantNotFoundException Caso não encontre o restaurante com o email especificado
+     */
+    public List<CustomerOrderResponse> getOrders(String email) {
+        Optional<Restaurant> restaurantOpt = restaurantRepository.findByEmail(email);
+
+        Restaurant restaurant = restaurantOpt.orElseThrow(() ->
+            new RestaurantNotFoundException("Restaurante não encontrado com o email: " + email)
+        );
+
+        List<CustomerOrder> orders = customerOrderRepository.findByRestaurantId(restaurant.getId());
+
+        return orders.stream()
+            .map(CustomerOrderResponse::new)
+            .toList();
+    }
 }
