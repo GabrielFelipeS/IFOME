d7a619cbc67200ecd581196efcc6125be6727f9c
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-15 18:32:30 -0300

    feat: findRefuseCustomerOrderByCustomerOrderId in RefuseCustomerOrderRepository

--- back/src/main/java/br/com/ifsp/ifome/repositories/RefuseCustomerOrderRepository.java
+++ back/src/main/java/br/com/ifsp/ifome/repositories/RefuseCustomerOrderRepository.java
@@ -3,5 +3,8 @@ package br.com.ifsp.ifome.repositories;
 import br.com.ifsp.ifome.entities.RefuseCustomerOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RefuseCustomerOrderRepository extends CrudRepository<RefuseCustomerOrder, Long> {
+    List<RefuseCustomerOrder> findRefuseCustomerOrderByCustomerOrderId(Long id);
 }
