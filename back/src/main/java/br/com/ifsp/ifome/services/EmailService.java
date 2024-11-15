0b61dfaf85449e614dbe67692c74d53a789b0ada
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-15 17:04:21 -0300

    docs: EmailService

--- back/src/main/java/br/com/ifsp/ifome/services/EmailService.java
+++ back/src/main/java/br/com/ifsp/ifome/services/EmailService.java
@@ -1,5 +1,6 @@
 package br.com.ifsp.ifome.services;
 
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +16,13 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String from;
 
+    /**
+     * Envia um email usando a conta de {@code  spring.mail.username}
+     *
+     * @param to Para quem enviar o email
+     * @param subject Assuno do email
+     * @param body O corpo do email
+     */
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
@@ -25,7 +33,13 @@ public class EmailService {
         mailSender.send(message);
     }
 
-    public void sendEmail(MimeMessage message) {
-        mailSender.send(message);
+    /**
+     * Envia um email usando a conta de {@code  spring.mail.username}
+     *
+     * @param mimeMessage Informações do email, incluindo destino, assunto e corpo
+     */
+    public void sendEmail(MimeMessage mimeMessage) throws MessagingException {
+        mimeMessage.setFrom(from);
+        mailSender.send(mimeMessage);
     }
 }
