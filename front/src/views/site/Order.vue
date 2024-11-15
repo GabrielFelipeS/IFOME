c8b06ef379ef86d8febdaffb271129af77520e55
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-11-15 14:04:41 -0300

    feat: add Order internal view

--- front/src/views/site/Order.vue
+++ front/src/views/site/Order.vue
@@ -1,11 +1,158 @@
 <script setup>
+import FooterMobile from "@/components/site/FooterMobile.vue";
+import CardOrder from "@/components/site/CardOrder.vue";
+import Header from "@/components/site/Header.vue";
+import {useRoute, useRouter} from "vue-router";
+import api from "@/services/api.js";
+import {onMounted, ref} from "vue";
+import {formatReal} from "@/services/formatReal.js";
 
+const router = useRouter();
+const route = useRoute();
+const orderId = route.params.id;
+
+const order = ref({});
+const orderItems = ref([]);
+
+const dropdown = ref(false);
+
+const verifyLogin = async () => {
+	const response = await api.post("auth/token/client/");
+	return response.status === 200;
+}
+const getOrder = async () => {
+	const login = await verifyLogin();
+	if (!login) {
+		return false;
+	}
+	const response = await api.get('order/customerOrders');
+	if (response.status !== 200) {
+		return false;
+	}
+	return response.data.find(order => order.orderId = orderId);
+}
+
+onMounted(async () => {
+	order.value = await getOrder();
+	orderItems.value = order.value.orderItems;
+});
+
+console.log(order)
 </script>
 
 <template>
-
+	<Header class="hidden md:flex"/>
+	<div class="w-full h-svh flex flex-row justify-center items-center pb-[75px]">
+		<div class="main">
+			<div class="flex flex-row h-[60px] justify-between items-center px-5
+				md:justify-start ">
+				<button class="md:hidden" @click="router.push('/orders')">
+					<v-icon name="fa-chevron-left" scale="1.8" class="text-primary md:hidden"/>
+				</button>
+				<button class="font-bold text-primary mr-4 flex items-center gap-1">
+					<v-icon name="fa-regular-question-circle" scale="1.3" class="md:hidden"/>
+					Ajuda
+				</button>
+			</div>
+			<div class="flex flex-col items-start justify-center px-5 gap-3">
+				<span class="text-tertiary-light font-semibold">Previsão de entrega</span>
+				<span class="text-lg font-bold -mt-4">Hoje, 19:12 - 19:27</span>
+				<div class="bg-green-700 h-1 rounded-full transition-all duration-300 w-full"></div>
+			</div>
+			<div class="flex flex-col items-start justify-center px-5 gap-3 py-4">
+				<button class="flex flex-row items-center w-full justify-between gap-4" @click="dropdown = !dropdown">
+					<v-icon name="fa-circle" scale="1" class="text-green-600"/>
+					<span class="text-xl font-semibold w-full self-start text-start leading-none">Joardo está indo ao restaurante</span>
+					<v-icon name="fa-chevron-down" scale="1.5" class="text-tertiary-light"/>
+				</button>
+				<transition
+					name="dropdown"
+					enter-active-class="transition ease-out duration-100"
+					enter-from-class="transform opacity-0 -translate-y-5"
+					enter-to-class="transform opacity-100 translate-y-0"
+					leave-active-class="transition ease-in "
+					leave-from-class="transform opacity-100 translate-y-0"
+					leave-to-class="transform opacity-0 -translate-y-5"
+				>
+					<div class="dropdown" v-if="dropdown">
+						<div class="flex flex-row justify-between">
+							<div class="flex flex-row items-center w-full justify-between gap-4 font-semibold">
+								<v-icon name="fa-circle" scale="0.75" class="text-green-600 ml-0.5"/>
+								<span class="text-tertiary-light w-full self-start text-start leading-none">Pedido pronto</span>
+								<span class="text-tertiary-light text-sm pr-1">18:17</span>
+							</div>
+						</div>
+						<div class="flex flex-row justify-between">
+							<div class="flex flex-row items-center w-full justify-between gap-4 font-semibold">
+								<v-icon name="fa-circle" scale="0.75" class="text-green-600 ml-0.5"/>
+								<span class="text-tertiary-light w-full self-start text-start leading-none">Pedido confirmado</span>
+								<span class="text-tertiary-light text-sm pr-1">18:17</span>
+							</div>
+						</div>
+						<div class="flex flex-row justify-between">
+							<div class="flex flex-row items-center w-full justify-between gap-4 font-semibold">
+								<v-icon name="fa-circle" scale="0.75" class="text-green-600 ml-0.5"/>
+								<span class="text-tertiary-light w-full self-start text-start leading-none">Pedido enviado ao restaurante</span>
+								<span class="text-tertiary-light text-sm pr-1">18:17</span>
+							</div>
+						</div>
+					</div>
+				</transition>
+			</div>
+			<div class="px-3 flex flex-col gap-y-0 mt-6 max-h-[35%]" :class="{ 'opacity-30': dropdown }">
+				<span class="font-semibold px-1">Resumo do pedido</span>
+				<div class="order-items border-s border-e px-3">
+					<div class="product" v-for="item in orderItems">
+						<div class="flex flex-col items-start gap-1.5">
+							<span class="uppercase">{{ item.dish.name }}</span>
+						</div>
+						<div class="mr-6 flex flex-col items-end">
+							<span>R$ {{ formatReal(item.unitPrice) }}</span>
+							<span>x {{ item.quantity }}</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-col h-full justify-end pb-3" :class="{ 'opacity-30': dropdown }">
+				<div class="order-summary my-8">
+					<div class="flex flex-row justify-between font-semibold">
+						<span>Total</span>
+						<span>R$ {{ formatReal(order.orderPrice) }}</span>
+					</div>
+				</div>
+				<div class="px-6 flex flex-row justify-between items-center">
+					<span class="text-sm text-tertiary-light font-semibold">Pagamento: {{ order.paymentStatus }}</span>
+					<span class="font-semibold text-sm flex items-center gap-1">
+						<v-icon name="md-pix" />
+						PIX
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<FooterMobile />
 </template>
 
 <style scoped>
+	.main {
+		@apply relative w-full h-full flex flex-col max-w-[1200px] justify-center self-center gap-y-4 overflow-auto pt-4;
 
+		@apply md:mt-[85px];
+	}
+	.order-items {
+		@apply flex flex-col overflow-y-scroll h-full;
+		@apply divide-y-4;
+
+	}
+	.product {
+		@apply flex flex-row justify-between py-4 -mt-1;
+	}
+	.order-summary {
+		@apply flex flex-col gap-4 px-6 font-semibold max-h-[20%];
+	}
+	.dropdown {
+		@apply absolute right-0 flex flex-col gap-4 mt-52 px-5 w-full z-50;
+		@apply bg-white border-b pb-4 shadow-xl;
+		@apply transition duration-500 ease-in-out transform;
+	}
 </style>
