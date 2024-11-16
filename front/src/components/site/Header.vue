03286cfad9d63bb10fb04a64a4e0caa93b07faa5
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-11-15 21:11:29 -0300

    feat: add user profile dropdown

--- front/src/components/site/Header.vue
+++ front/src/components/site/Header.vue
@@ -22,9 +22,33 @@
                 <p class="text-xs text-tertiary-light">{{ cart.totalItems }} Itens</p>
             </div>
         </div>
-        <div class="hidden md:block">
-            <v-icon name="fa-sign-out-alt" scale="1.5" class="text-primary cursor-pointer" />
-        </div>
+		<div class="relative flex-row hidden md:flex">
+			<!-- Ícone com evento de click -->
+			<v-icon name="fa-user-circle" scale="1.8" class="text-primary cursor-pointer" @click="toggleDropdown" />
+			<!-- Dropdown -->
+			<transition
+				name="dropdown"
+				enter-active-class="transition ease-out duration-100"
+				enter-from-class="transform opacity-0 -translate-y-5"
+				enter-to-class="transform opacity-100 translate-y-0"
+				leave-active-class="transition ease-in "
+				leave-from-class="transform opacity-100 translate-y-0"
+				leave-to-class="transform opacity-0 -translate-y-5"
+			>
+				<ul v-if="isDropdownOpen" class="absolute top-full -right-6 mt-2 w-48 text-lg text-tertiary-light
+						bg-white border border-gray-300 rounded-md shadow-2xl" >
+					<li class="px-4 py-2 hover:bg-gray-100 cursor-pointer flex items-center gap-4"
+						@click="router.push('/orders')">
+						<v-icon name="fa-receipt" scale="1.1"/>
+						Pedidos
+					</li>
+					<li class="px-4 py-2 hover:bg-gray-100 cursor-pointer flex items-center gap-4">
+						<v-icon name="fa-sign-out-alt" scale="1.1" class="cursor-pointer" />
+						Sair
+					</li>
+				</ul>
+			</transition>
+		</div>
     </header>
 </template>
 
@@ -34,6 +58,7 @@ import { useCart } from "@/stores/cart.js";
 import api from "@/services/api.js";
 import {useToast} from "vue-toast-notification";
 import {formatReal} from "@/services/formatReal.js";
+import router from "@/router/index.js";
 
 const emit = defineEmits(['search', 'open-cart'])
 
@@ -64,4 +89,9 @@ const updateCart = async () => {
 onMounted(() => {
 	updateCart();
 })
+
+const isDropdownOpen = ref(false);
+const toggleDropdown = () => {
+	isDropdownOpen.value = !isDropdownOpen.value;
+};
 </script>
