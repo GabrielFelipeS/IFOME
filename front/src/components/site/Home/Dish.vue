0ae2c60a87002e565361436d012d71213bb9740d
Micael Hernandes <micael.hernandes@bloomin.com>
2024-11-17 14:42:27 -0300

    feat: add link to show more Dishs and to show details in internal restaurant page

--- front/src/components/site/Home/Dish.vue
+++ front/src/components/site/Home/Dish.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="w-[90%] md:max-w-[1200px] mx-auto flex flex-col md:p-5">
+  <div class="w-[90%] md:max-w-[1200px] mx-auto flex flex-col md:p-5 mb-[100px] md:mb-0">
     <h2 class="text-2xl mt-2 ml-2 font-normal text-tertiary-light">Pratos</h2>
 
-    <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 mt-5">
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 mt-5 md:mb-0">
       <template v-if="dishes.length > 0">
-        <DishCard
-            v-for="dish in limitedDishes" :key="dish.id"
-            :dish="dish"
-        />
+        <router-link :to="{name:'restaurant internal', params: {id: dish.restaurantId }, query: { dish: dish.id }}" v-for="dish in limitedDishes" :key="$route.fullPath">
+          <DishCard
+               :key="dish.id"
+              :dish="dish"
+          />
+        </router-link>
       </template>
 
       <template v-else>
         <p class="col-span-full text-center">Nenhum restaurante encontrado</p>
       </template>
     </div>
+    <router-link v-if="dishes.length > 0" :to="{name : 'dishs'}"
+                 class="w-full h-[50px] bg-white border border-tertiary-light text-center rounded-md text-primary flex justify-center items-center mt-5 md:mt-10 hover:rounded-md duration-150 hover:transition-all hover:-translate-y-1 hover:text-white hover:bg-primary hover:border-none">
+      Ver todos
+    </router-link>
   </div>
 </template>
 
@@ -31,7 +37,6 @@ onMounted(async () => {
   try {
     const {data} = await axios.get(`${import.meta.env.VITE_API_URL}dish/all`);
     dishes.value = data.data || [];
-    console.log(dishes.value);
   } catch (error) {
     console.error("Erro ao buscar os restaurantes:", error);
     dishes.value = [];
