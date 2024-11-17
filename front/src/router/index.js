cb7ff829bbe6143a50462e5583c324ff3e1568e5
Micael Hernandes <micael.hernandes@bloomin.com>
2024-11-16 22:41:33 -0300

    feat: create guard to check current token

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,13 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Register from '@/views/store/Register.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Register from "@/views/store/Register.vue";
+import api from "@/services/api";
 
 // Definindo as rotas com prefixos aninhados
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
+      beforeEnter: async (to, from, next) => {
+        try {
+          const data = await api.post(
+            `${import.meta.env.VITE_API_URL}auth/token/client/`
+          );
+          if (data.status === 200) {
+            next();
+          } else {
+            next({ name: "user-login" });
+          }
+        } catch (e) {
+          localStorage.removeItem("token");
+          console.error(e);
+          next({ name: "user-login" });
+        }
+      },
       children: [
         {
           path: '/restaurants',
@@ -15,91 +32,91 @@ const router = createRouter({
           component: () => import('@/views/site/Restaurants.vue'),
         },
         {
-          path: '/restaurant/:id',
-          name: 'restaurant internal',
-          component: () => import('@/views/site/Restaurant.vue'),
+          path: "/restaurant/:id",
+          name: "restaurant internal",
+          component: () => import("@/views/site/Restaurant.vue"),
         },
         {
-          path: '/orders',
-          name: 'orders',
-          component: () => import('@/views/site/Orders.vue'),
+          path: "/orders",
+          name: "orders",
+          component: () => import("@/views/site/Orders.vue"),
         },
         {
-          path: '/order/:id',
-          name: 'order internal',
-          component: () => import('@/views/site/Order.vue'),
+          path: "/order/:id",
+          name: "order internal",
+          component: () => import("@/views/site/Order.vue"),
         },
-      ]
+      ],
     },
     {
-      path: '/store',
+      path: "/store",
       children: [
         {
-          path: 'register',
-          name: 'store-register',
+          path: "register",
+          name: "store-register",
           component: Register,
         },
         {
-          path: 'login',
-          name: 'store-login',
-          component: () => import('@/views/store/Login.vue'),
+          path: "login",
+          name: "store-login",
+          component: () => import("@/views/store/Login.vue"),
         },
         {
-          path: 'new-dish',
-          name: 'store-new-dish',
-          component: () => import('@/views/store/NewDish.vue'),
+          path: "new-dish",
+          name: "store-new-dish",
+          component: () => import("@/views/store/NewDish.vue"),
         },
         {
-          path: 'panel',
-          name: 'store-panel',
-          component: () => import('@/views/store/Panel.vue'),
+          path: "panel",
+          name: "store-panel",
+          component: () => import("@/views/store/Panel.vue"),
         },
       ],
     },
     {
-      path: '/user',
+      path: "/user",
       children: [
         {
-          path: 'register',
-          name: 'user-register',
-          component: () => import('../views/user/Register.vue'),
+          path: "register",
+          name: "user-register",
+          component: () => import("../views/user/Register.vue"),
         },
         {
-          path: 'login',
-          name: 'user-login',
-          component: () => import('../views/user/Login.vue'),
+          path: "login",
+          name: "user-login",
+          component: () => import("../views/user/Login.vue"),
         },
       ],
     },
     {
-      path: '/delivery',
+      path: "/delivery",
       children: [
         {
-          path: 'register',
-          name: 'delivery-register',
-          component: () => import('../views/delivery/Register.vue'),
+          path: "register",
+          name: "delivery-register",
+          component: () => import("../views/delivery/Register.vue"),
         },
         {
-          path: 'login',
-          name: 'delivery-login',
-          component: () => import('../views/delivery/Login.vue'),
+          path: "login",
+          name: "delivery-login",
+          component: () => import("../views/delivery/Login.vue"),
         },
         {
-          path: 'panel',
-          name: 'delivery-panel',
-          component: () => import('../views/delivery/Home.vue'),
-        }
+          path: "panel",
+          name: "delivery-panel",
+          component: () => import("../views/delivery/Home.vue"),
+        },
       ],
     },
     {
-      path: '/NotFound',
-      name: 'NotFound',
-      component: () => import('@/views/site/NotFound.vue'),
+      path: "/NotFound",
+      name: "NotFound",
+      component: () => import("@/views/site/NotFound.vue"),
     },
     {
-      path: '/:pathMatch(.*)*',
-      redirect: { name: 'NotFound' },
-    }
+      path: "/:pathMatch(.*)*",
+      redirect: { name: "NotFound" },
+    },
   ],
 });
 
