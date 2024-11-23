34d2be51ae8423e4d8f06891cecdbe9a962ea8dc
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-11-23 12:12:48 -0300

    feat: DeliveryChatCannotBeOpenedException

--- back/src/main/java/br/com/ifsp/ifome/exceptions/global/DeliveryChatCannotBeOpenedException.java
+++ back/src/main/java/br/com/ifsp/ifome/exceptions/global/DeliveryChatCannotBeOpenedException.java
@@ -2,6 +2,6 @@ package br.com.ifsp.ifome.exceptions.global;
 
 public class DeliveryChatCannotBeOpenedException extends RuntimeException{
     public DeliveryChatCannotBeOpenedException(String message) {
-
+        super(message);
     }
 }
