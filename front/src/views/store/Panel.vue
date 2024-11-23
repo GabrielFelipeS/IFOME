4afe27fc525d3ccd7bcdc0c6c65d2981f7be4d83
Micael Hernandes <micael.hernandes@bloomin.com>
2024-11-23 11:04:37 -0300

    wip: token validate on enter in panel page

--- front/src/views/store/Panel.vue
+++ front/src/views/store/Panel.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="flex flex-row" v-if="!isMobile">
-        <SidebarMenu />
+        <SidebarMenu/>
         <main class="h-[100vh] w-[calc(100vw_-_150px)] flex flex-row">
-            <Orders :orders="orders" @click="showOrderDetails" />
-            <div class="h-[100vh] lg:w-[calc(100vw_-_450px)] md:w-[calc(100vw_-_250px)] flex flex-col" v-if="currentOrder">
-                <OrderHeader :order="currentOrder" />
+            <Orders :orders="orders" @click="showOrderDetails"/>
+            <div class="h-[100vh] lg:w-[calc(100vw_-_450px)] md:w-[calc(100vw_-_250px)] flex flex-col"
+                 v-if="currentOrder">
+                <OrderHeader :order="currentOrder"/>
                 <div class="w-full h-[60vh] grid grid-cols-2">
-                    <OrderItems :order="currentOrder" />
-                    <OrderChat />
+                    <OrderItems :order="currentOrder"/>
+                    <OrderChat/>
                 </div>
-                <OrderStatus :order="currentOrder" />
+                <OrderStatus :order="currentOrder"/>
             </div>
 
-            <div class="h-[100vh] lg:w-[calc(100vw_-_450px)] md:w-[calc(100vw_-_250px)] flex flex-col items-center justify-center"
+            <div
+                class="h-[100vh] lg:w-[calc(100vw_-_450px)] md:w-[calc(100vw_-_250px)] flex flex-col items-center justify-center"
                 v-if="!currentOrder">
                 <h1 class="text-3xl text-tertiary-subtle">
                     Selecione um pedido para visualizar detalhes
@@ -22,7 +24,8 @@
     </div>
 
     <div v-else class="h-[100vh] flex items-center justify-center text-center p-4 bg-red-100">
-        <h1 class="text-2xl font-semibold text-red-600">Este painel não é adequado para dispositivos móveis ou com resolução menor que 1280px. Por favor, utilize em um dispositivo maior.</h1>
+        <h1 class="text-2xl font-semibold text-red-600">Este painel não é adequado para dispositivos móveis ou com
+            resolução menor que 1280px. Por favor, utilize em um dispositivo maior.</h1>
     </div>
 </template>
 
@@ -34,8 +37,8 @@ import Orders from '@/components/store/panel/Orders.vue';
 import OrderStatus from '@/components/store/panel/OrderStatus.vue';
 import SidebarMenu from '@/components/store/panel/sidebarMenu.vue';
 import api from '@/services/api';
-import { computed, ref, onMounted, onUnmounted } from 'vue';
-import { useRouter } from 'vue-router';
+import {computed, ref, onMounted, onUnmounted} from 'vue';
+import {useRouter} from 'vue-router';
 
 const orders = ref([]);
 const selectedOrderId = ref(null);
@@ -48,10 +51,15 @@ function updateWindowWidth() {
 
 const isMobile = computed(() => windowWidth.value < 1280);
 
-onMounted(async () => {
-    window.addEventListener('resize', updateWindowWidth);
+onMounted(() => {
+    window.addEventListener('resize', updateWindowWidth)
+});
+
+// checkIfUserIsAuthenticated();
+
+async function checkIfUserIsAuthenticated() {
     try {
-        const { data } = await api.post('auth/token/restaurant/' ,{
+        const {data} = await api.post('auth/token/restaurant/', {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`,
             },
@@ -60,12 +68,12 @@ onMounted(async () => {
     } catch (error) {
         if (error.response && error.response.status === 401) {
             localStorage.removeItem('token');
-            router.push({ name: 'store-login' });
+            router.push({name: 'store-login'});
         } else {
             console.error('Erro ao buscar pedidos:', error);
         }
     }
-});
+}
 
 onUnmounted(() => {
     window.removeEventListener('resize', updateWindowWidth);
