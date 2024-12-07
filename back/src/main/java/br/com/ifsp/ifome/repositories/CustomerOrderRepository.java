837d28591fa2ee389a45150df84d193889c71abc
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-12-07 16:44:35 -0300

    feat: findAllByCartClientEmail and findAllByCartClientEmailAndCurrentOrderClientStatus

--- back/src/main/java/br/com/ifsp/ifome/repositories/CustomerOrderRepository.java
+++ back/src/main/java/br/com/ifsp/ifome/repositories/CustomerOrderRepository.java
@@ -1,16 +1,19 @@
 package br.com.ifsp.ifome.repositories;
 
 import br.com.ifsp.ifome.entities.CustomerOrder;
+import br.com.ifsp.ifome.entities.OrderClientStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long>
 {
-    List<CustomerOrder> findAllByCartClientEmail(String email);
+    List<CustomerOrder> findAllByCartClientEmailAndCurrentOrderClientStatus(String email, OrderClientStatus orderClientStatus);
+
+    @Query("SELECT co FROM CustomerOrder co WHERE co.currentOrderClientStatus != 'CONCLUIDO' AND co.cart.client.email = :email")
+    List<CustomerOrder> findAllByCartClientEmail(@Param("email") String email);
 
     @Query("SELECT c FROM CustomerOrder c WHERE c.restaurant.id = :id ORDER BY c.orderDate desc")
     List<CustomerOrder> findByRestaurantId(@Param("id") Long restaurantId);
@@ -20,5 +23,4 @@ public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Lo
 
     @Query("SELECT c FROM CustomerOrder c WHERE c.deliveryPerson.email = :email AND c.currentOrderClientStatus != 'CONCLUIDO'")
     List<CustomerOrder> findActiveOrderDeliveryPerson(String email);
-
 }
