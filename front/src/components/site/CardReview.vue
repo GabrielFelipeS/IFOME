beddcb5ed24e0b62f103c80a28780ca64d372480
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-12-07 13:31:05 -0300

    style: comment size

--- front/src/components/site/CardReview.vue
+++ front/src/components/site/CardReview.vue
@@ -10,16 +10,16 @@ console.log(props.review);
 
 <template>
 	<main class="flex justify-center mt-10">
-		<div class="max-w-sm w-full bg-white shadow-lg rounded-lg p-6 border border-gray-200">
+		<div class="max-w-sm w-full bg-white shadow-lg rounded-lg p-6 border border-gray-200 flex flex-col justify-between">
 			<!-- Comentário -->
-			<div class="mb-4">
-				<span class="text-gray-700 text-lg font-medium">{{ review.comment }}</span>
+			<div>
+				<span class="text-gray-700">{{ review.comment }}</span>
 			</div>
 
 			<!-- Estrelas e Avaliação -->
-			<div class="flex items-center space-x-2">
+			<div class="flex items-center space-x-2 mt-4 self-start">
 				<v-icon name="fa-star" class="text-yellow-400" />
-				<span class="text-gray-600 text-sm">{{ review.stars.toFixed(0) }}</span>
+				<span class="text-gray-600 text-sm font-bold">{{ review.stars.toFixed(0) }}</span>
 			</div>
 		</div>
 	</main>
