cc84e0587be35c24cc00f4d61d97e776b1fd4d19
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-12-07 18:59:52 -0300

    feat: add dish and restaurant recommendation to home view

--- front/src/views/site/Home.vue
+++ front/src/views/site/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <PromoCards/>
-  <Restaurants/>
-  <Dish/>
+	<PromoCards/>
+	<RestaurantRecommendations />
+	<DishRecommendations />
+	<Restaurants/>
+	<Dish/>
 </template>
 
 <script setup>
 import PromoCards from "@/components/site/Home/PromoCards.vue";
 import Restaurants from "@/components/site/Home/Restaurants.vue";
 import Dish from "@/components/site/Home/Dish.vue";
+import RestaurantRecommendations from "@/components/site/Home/RestaurantRecommendations.vue";
+import DishRecommendations from "@/components/site/Home/DishRecommendations.vue";
 </script>
